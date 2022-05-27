7c51c713f8b5247226006c53bb2abeeec8d0bb51
danielmonyak <daniel.monyak@arvinas.com>
2022-05-27 20:00:01 +0000

    update

---
+++ data/masif_ligand/schedule_data_prepare.sh
@@ -0,0 +1,21 @@
+i=0
+batchSize=8
+unset running
+while read p; do
+	if [ $(( i % $batchSize )) -eq 0 ]; then
+		for pid in ${running[@]}; do
+			while ps -o pid ax | grep -q $pid; do
+				sleep 60
+			done
+		done
+		running=()
+	fi
+	FIELD1=$(echo $p| cut -d" " -f1)
+       	PDBID=$(echo $FIELD1| cut -d"_" -f1)
+      	CHAIN1=$(echo $FIELD1| cut -d"_" -f2)
+       	CHAIN2=$(echo $FIELD1| cut -d"_" -f3)
+ 	./data_prepare_one.sh $PDBID\_$CHAIN1\_$CHAIN2 > output_files/$p.out 2>error_files/$p.err &
+	disown -h $!
+	running+=($!)
+	i=$((i+1))
+done < lists/sequence_split_list.txt
--- source/default_config/masif_opts.py
+++ source/default_config/masif_opts.py
@@ -1,10 +1,12 @@
 import tempfile
 
+basedir = "/data02/daniel/masif/"
+
 masif_opts = {}
 # Default directories
-masif_opts["raw_pdb_dir"] = "data_preparation/00-raw_pdbs/"
-masif_opts["pdb_chain_dir"] = "data_preparation/01-benchmark_pdbs/"
-masif_opts["ply_chain_dir"] = "data_preparation/01-benchmark_surfaces/"
+masif_opts["raw_pdb_dir"] = basedir + "data_preparation/00-raw_pdbs/"
+masif_opts["pdb_chain_dir"] = basedir + "data_preparation/01-benchmark_pdbs/"
+masif_opts["ply_chain_dir"] = basedir + "data_preparation/01-benchmark_surfaces/"
 masif_opts["tmp_dir"] = tempfile.gettempdir()
 masif_opts["ply_file_template"] = masif_opts["ply_chain_dir"] + "/{}_{}.ply"
 
@@ -29,7 +31,7 @@ masif_opts["ppi_search"]["max_shape_size"] = 200
 masif_opts["ppi_search"]["max_distance"] = 12.0  # Radius for the neural network.
 masif_opts["ppi_search"][
     "masif_precomputation_dir"
-] = "data_preparation/04b-precomputation_12A/precomputation/"
+] = basedir + "data_preparation/04b-precomputation_12A/precomputation/"
 masif_opts["ppi_search"]["feat_mask"] = [1.0] * 5
 masif_opts["ppi_search"]["max_sc_filt"] = 1.0
 masif_opts["ppi_search"]["min_sc_filt"] = 0.5
@@ -54,7 +56,7 @@ masif_opts["site"]["n_conv_layers"] = 3
 masif_opts["site"]["max_distance"] = 9.0  # Radius for the neural network.
 masif_opts["site"][
     "masif_precomputation_dir"
-] = "data_preparation/04a-precomputation_9A/precomputation/"
+] = basedir + "data_preparation/04a-precomputation_9A/precomputation/"
 masif_opts["site"]["range_val_samples"] = 0.9  # 0.9 to 1.0
 masif_opts["site"]["model_dir"] = "nn_models/all_feat_3l/model_data/"
 masif_opts["site"]["out_pred_dir"] = "output/all_feat_3l/pred_data/"
@@ -63,17 +65,17 @@ masif_opts["site"]["feat_mask"] = [1.0] * 5
 
 # Neural network ligand application specific parameters.
 masif_opts["ligand"] = {}
-masif_opts["ligand"]["assembly_dir"] = "data_preparation/00b-pdbs_assembly"
-masif_opts["ligand"]["ligand_coords_dir"] = "data_preparation/00c-ligand_coords"
+masif_opts["ligand"]["assembly_dir"] = basedir + "data_preparation/00b-pdbs_assembly"
+masif_opts["ligand"]["ligand_coords_dir"] = basedir + "data_preparation/00c-ligand_coords"
 masif_opts["ligand"][
     "masif_precomputation_dir"
-] = "data_preparation/04a-precomputation_12A/precomputation/"
+] = basedir + "data_preparation/04a-precomputation_12A/precomputation/"
 masif_opts["ligand"]["max_shape_size"] = 200
 masif_opts["ligand"]["feat_mask"] = [1.0] * 5
 masif_opts["ligand"]["train_fract"] = 0.9 * 0.8
 masif_opts["ligand"]["val_fract"] = 0.1 * 0.8
 masif_opts["ligand"]["test_fract"] = 0.2
-masif_opts["ligand"]["tfrecords_dir"] = "data_preparation/tfrecords"
+masif_opts["ligand"]["tfrecords_dir"] = basedir + "data_preparation/tfrecords"
 masif_opts["ligand"]["max_distance"] = 12.0
 masif_opts["ligand"]["n_classes"] = 7
 masif_opts["ligand"]["feat_mask"] = [1.0, 1.0, 1.0, 1.0, 1.0]
