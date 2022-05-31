56a6270e67b6396a36a26d857f6f1125e8ba40d9
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-05-31 18:43:50 -0400

    Update masif_ligand_train_new.py

--- source/masif_ligand/masif_ligand_train_new.py
+++ source/masif_ligand/masif_ligand_train_new.py
@@ -37,7 +37,7 @@ if not os.path.exists(params["model_dir"]):
     os.makedirs(params["model_dir"])
 
 # Edited by Daniel Monyak
-config = tf.ConfigProto(allow_soft_placement=True)
+config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)
 config.gpu_options.allow_growth = True
 #with tf.Session() as sess:
 with tf.Session(config) as sess:
