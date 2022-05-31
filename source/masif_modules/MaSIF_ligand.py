1cbf363b8d0fdcc2fe6026ba5f45f54ea6c6910e
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-05-31 18:51:57 -0400

    Update MaSIF_ligand.py

--- source/masif_modules/MaSIF_ligand.py
+++ source/masif_modules/MaSIF_ligand.py
@@ -3,8 +3,8 @@ import numpy as np
 
 # Edited by Daniel Monyak
 tf.debugging.set_log_device_placement(True)
-gpus = tf.compat.v1.config.experimental.list_logical_devices('GPU')
-#gpus = [g.name for g in gpus]
+gpus = tf.compat.v1.config.experimental.list_physical_devices('GPU')
+gpus = [g.name for g in gpus]
 #strategy = tf.distribute.MirroredStrategy(gpus)
 
 
