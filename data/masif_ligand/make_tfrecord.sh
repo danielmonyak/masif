20e26b49a146f28e08d0ff7a2d3765e9a14aca29
danielmonyak <daniel.monyak@arvinas.com>
2022-06-01 14:16:44 +0000

    deleted file

--- data/masif_ligand/make_tfrecord.sh
+++ data/masif_ligand/make_tfrecord.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-#SBATCH --nodes 1
-#SBATCH --ntasks 1
-#SBATCH --cpus-per-task 1
-#SBATCH --mem 8192
-#SBATCH --time 48:00:00
-#SBATCH --partition=serial
-
-
 source /apps01/anaconda3/etc/profile.d/conda.sh
 #conda activate venv_latest
 conda activate venv_tf19
@@ -16,4 +7,6 @@ masif_source=$masif_root/source/
 masif_matlab=$masif_root/source/matlab_libs/
 export PYTHONPATH=$PYTHONPATH:$masif_source
 
-python -u $masif_source/data_preparation/04b-make_ligand_tfrecords.py
+python -u $masif_source/data_preparation/04b-make_ligand_tfrecords.py >  make_tf.out 2>make_tf.err &
+disown -h $!
+echo $! > make_tfrecord_pid.txt
