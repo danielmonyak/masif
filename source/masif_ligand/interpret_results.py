75ef4bc44461af94900c1ab6a41dd22eb11c1d2b
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-02 11:48:19 -0400

    Update interpret_results.py

--- source/masif_ligand/interpret_results.py
+++ source/masif_ligand/interpret_results.py
@@ -21,32 +21,31 @@ testing_data = tf.data.TFRecordDataset(
 testing_data = testing_data.map(_parse_function)
 
 n_ligands = params["n_classes"]
-'''
-for ligand in range(n_ligands):
-  labels = np.loadtxt(test_set_out_dir + "{}_labels.npy".format(prot))
-  logits_softmax = np.loadtxt(test_set_out_dir + "{}_logits.npy".format(prot))
-'''
+saved_pdbs = np.loadtxt('saved_pdbs.txt')
 num_test_samples = 290
-with tf.Session() as sess:
+'''with tf.Session() as sess:
     testing_iterator = testing_data.make_one_shot_iterator()
     testing_next_element = testing_iterator.get_next()
-    for num_test_sample in range(num_test_samples):
-        print('\nnum_test_sample: ', num_test_sample)
-        try:
-            data_element = sess.run(testing_next_element)
-        except:
-            continue
 
-        pdb = data_element[5]
-        labels = np.load(test_set_out_dir + "{}_labels.npy".format(pdb)).astype(float)
-        print(labels)
-    '''
-    pdb = '4D86_A'
-    labels = np.load(test_set_out_dir + "b'{}'_labels.npy".format(pdb)).astype(float)
-    logits_softmax = np.load(test_set_out_dir + "b'{}'_logits.npy".format(prot)).astype(float)
-    .reshape([-1, n_ligands])
+for num_test_sample in range(num_test_samples):
+    print('\nnum_test_sample: ', num_test_sample)
+    try:
+        data_element = sess.run(testing_next_element)
+    except:
+        continue
 
+    pdb = data_element[5]
+'''
+for pdb in saved_pdbs:
+    labels = np.load(test_set_out_dir + "{}_labels.npy".format(pdb)).astype(float)
     print(labels)
-    print(logis_softmax)
-    '''
-    #conf_mat = confusion_matrix(y_true, y_pred)
+'''
+pdb = '4D86_A'
+labels = np.load(test_set_out_dir + "b'{}'_labels.npy".format(pdb)).astype(float)
+logits_softmax = np.load(test_set_out_dir + "b'{}'_logits.npy".format(prot)).astype(float)
+.reshape([-1, n_ligands])
+
+print(labels)
+print(logis_softmax)
+'''
+#conf_mat = confusion_matrix(y_true, y_pred)
