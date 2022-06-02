3d420c668d816f58b7cdd17e40bddfd1526c8324
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-02 09:52:28 -0400

    Create interpret_results.py

---
+++ source/masif_ligand/interpret_results.py
@@ -0,0 +1,16 @@
+import numpy as np
+from default_config.masif_opts import masif_opts
+from masif_modules.MaSIF_ligand_new import MaSIF_ligand
+from sklearn.metrics import confusion_matrix
+import tensorflow as tf
+
+
+params = masif_opts["ligand"]
+test_set_out_dir = params["test_set_out_dir"]
+
+n_ligands = 7
+
+for ligand in range(n_ligands):
+  labels = np.loadtxt(test_set_out_dir + "{}_labels.npy".format(something))
+  logits_softmax = np.loadtxt(test_set_out_dir + "{}_logits.npy".format(something))
+  
