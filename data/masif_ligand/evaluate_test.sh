91332369bf8da5ce7a97f845df5c63ad2f53a964
danielmonyak <daniel.monyak@arvinas.com>
2022-06-02 14:09:43 +0000

    update

--- data/masif_ligand/evaluate_test.sh
+++ data/masif_ligand/evaluate_test.sh
@@ -1,7 +1,12 @@
-#!/bin/bash -l
+source /apps01/anaconda3/etc/profile.d/conda.sh
+conda activate venv_tf1-15
+
 masif_root=$(git rev-parse --show-toplevel)
-masif_source=$masif_root/source/
-masif_matlab=$masif_root/source/matlab_libs/
+masif_source=$masif_root/source
 export PYTHONPATH=$PYTHONPATH:$masif_source
 
-python -u $masif_source/masif_ligand/masif_ligand_evaluate_test.py
+script=masif_ligand_evaluate_test_new.py 
+
+python -u $masif_source/masif_ligand/$script > evaluate_test.out 2>evaluate_test.err &
+disown -h $!
+echo $! > evaluate_test_pid.txt
