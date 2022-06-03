e85d78c0f0a73aec2475611576c63c975cc11e97
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-03 09:45:09 -0400

    Update view_empties.py

--- data/masif_ligand/view_empties.py
+++ data/masif_ligand/view_empties.py
@@ -27,53 +27,29 @@ testing_iterator = testing_data.make_one_shot_iterator()
 testing_next_element = testing_iterator.get_next()
 sess = tf.Session()
 
-'''
-data_element = sess.run(testing_next_element)
 
-labels = data_element[4]
-n_ligands = labels.shape[1]
-pdb_logits_softmax = []
-pdb_labels = []
-
-print('pdb: ', data_element[5])
-
-print('labels: ', labels)
-print('n_ligands: ', n_ligands)
-
-ligand = 0
-print('ligand: ', ligand)
-
-pocket_points = np.where(labels[:, ligand] != 0.0)[0]
-label = np.max(labels[:, ligand]) - 1
-
-print('label: ', label)
-
-pocket_labels = np.zeros(7, dtype=np.float32)
-pocket_labels[label] = 1.0
-npoints = pocket_points.shape[0]
-print('npoints', npoints)
-
-pdb_labels.append(label)
-pdb = data_element[5]
-'''
 bad_pdbs = ["b'4ZNL_A'", "b'3D36_ACB'", "b'2REQ_AB'", "b'4H6Q_A'", "b'4BLV_A'", "b'1X7P_AB'", "b'5L8J_AB'", "b'5HMN_CE'", "b'3UQD_ACBD'", "b'1XX6_AB'", "b'2OWM_AC'"]
 num_test_samples = 290
-splits = [0, int(num_test_samples/4), int(num_test_samples/2), int(3 * num_test_samples/4), num_test_samples]
-devices = ['/GPU:0', '/GPU:1', '/GPU:2', '/GPU:3']
+#splits = [0, int(num_test_samples/4), int(num_test_samples/2), int(3 * num_test_samples/4), num_test_samples]
+#devices = ['/GPU:0', '/GPU:1', '/GPU:2', '/GPU:3']
 bad_data_elements = []
-for i in range(len(devices)):
-    with tf.device(devices[i]):
-        print("Using ", devices[i])
-        for num_test_sample in range(splits[i], splits[i+1]):
-            print(num_test_sample)
-            try:
-                data_element = sess.run(testing_next_element)
-            except:
-                continue
+#for i in range(len(devices)):
+#    with tf.device(devices[i]):
+gpus = tf.compat.v1.config.experimental.list_logical_devices('GPU')
+gpus_str = [g.name for g in gpus]
+strategy = tf.distribute.MirroredStrategy(gpus_str)
+#print("Using ", devices[i])
+with strategy.scope():
+    #for num_test_sample in range(splits[i], splits[i+1]):
+    print(num_test_sample)
+    try:
+        data_element = sess.run(testing_next_element)
+    except:
+        continue
 
-            pdb = data_element[5]
-            if pdb in bad_pdbs:
-              bad_data_elements.append(data_elements)
+    pdb = data_element[5]
+    if pdb in bad_pdbs:
+      bad_data_elements.append(data_elements)
 '''
     labels = data_element[4]
     all_ligands = np.unique(labels.max(axis = 0))
