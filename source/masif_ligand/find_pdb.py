085114e5b491004ec00c3c6bb2584dd291045b5d
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-03 17:45:21 -0400

    Create find_pdb.py

---
+++ source/masif_ligand/find_pdb.py
@@ -0,0 +1,43 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+
+import numpy as np
+from IPython.core.debugger import set_trace
+import importlib
+import sys
+from default_config.masif_opts import masif_opts
+from masif_modules.read_ligand_tfrecords import _parse_function
+import tensorflow as tf
+
+params = masif_opts["ligand"]
+test_set_out_dir = params["test_set_out_dir"]
+n_ligands = params["n_classes"]
+
+# Load testing data
+testing_data = tf.data.TFRecordDataset(
+    os.path.join(params["tfrecords_dir"], "testing_data_sequenceSplit_30.tfrecord")
+)
+testing_data = testing_data.map(_parse_function)
+
+testing_iterator = testing_data.make_one_shot_iterator()
+testing_next_element = testing_iterator.get_next()
+sess = tf.Session()
+
+data_element = sess.run(testing_next_element)
+
+target_pdb = "b'1U5U_A'"
+
+num_test_samples = 290
+for num_test_sample in range(num_test_samples):
+    try:
+        data_element = sess.run(testing_next_element)
+    except:
+        continue
+
+    pdb = data_element[5]
+    if pdb != target_pdb:
+        continue
+    labels = data_element[4]
+    print(labels)
+    break
+sess.close()
