bd59693cc06c1561677fd8499da11e4215c1a48c
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-03 12:13:00 -0400

    Update interpret_results_prob.py

--- data/masif_ligand/analysis/interpret_results_prob.py
+++ data/masif_ligand/analysis/interpret_results_prob.py
@@ -19,66 +19,10 @@ n_ligands = params["n_classes"]
 
 saved_pdbs = np.loadtxt('saved_pdbs.txt', dtype='str')
 
-'''
-# Load testing data
-testing_data = tf.data.TFRecordDataset(
-    os.path.join(params["tfrecords_dir"], "testing_data_sequenceSplit_30.tfrecord")
-)
-testing_data = testing_data.map(_parse_function)
-
-testing_iterator = testing_data.make_one_shot_iterator()
-testing_next_element = testing_iterator.get_next()
-sess = tf.Session()
-'''
-'''
-data_element = sess.run(testing_next_element)
-
-labels = data_element[4]
-n_ligands = labels.shape[1]
-pdb_logits_softmax = []
-pdb_labels = []
-
-print('pdb: ', data_element[5])
-
-print('labels: ', labels)
-print('n_ligands: ', n_ligands)
-
-ligand = 0
-print('ligand: ', ligand)
-
-pocket_points = np.where(labels[:, ligand] != 0.0)[0]
-label = np.max(labels[:, ligand]) - 1
-
-print('label: ', label)
-
-pocket_labels = np.zeros(7, dtype=np.float32)
-pocket_labels[label] = 1.0
-npoints = pocket_points.shape[0]
-print('npoints', npoints)
-
-pdb_labels.append(label)
-pdb = data_element[5]
-'''
-'''
-num_test_samples = 290
-for num_test_sample in range(num_test_samples):
-    try:
-        data_element = sess.run(testing_next_element)
-    except:
-        continue
-
-    pdb = data_element[5]
-    labels = data_element[4]
-    all_ligands = np.unique(labels.max(axis = 0))
-    if len(all_ligands) != 1:
-        print('pdb: ', pdb)
-        print(all_ligands)
-sess.close()
-'''
-
 y_true = []
 y_pred = []
 zero_dim = 0
+'''
 for pdb in saved_pdbs:
     labels = np.load(test_set_out_dir + "{}_labels.npy".format(pdb)).astype(float)
     if labels.shape[0] == 0:
@@ -98,14 +42,11 @@ for pdb in saved_pdbs:
     total_freqs = pd.concat(df_list).groupby(0).sum()[1]
     y_pred.append(total_freqs.idxmax())
     
-
+'''
 #.reshape([-1, n_ligands])
 '''conf_mat = confusion_matrix(y_true, y_pred, normalize = 'true')
 disp = ConfusionMatrixDisplay(conf_mat)
 disp.plot()
 plt.savefig('confusion_matrix.png')
-'''
 print(balanced_accuracy_score(y_true, y_pred))
-
-#sess.close()
-
+#
