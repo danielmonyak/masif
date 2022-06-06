a78b92e76473802166e1660c6c59ebee5b57fd45
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-06 09:26:47 -0400

    Update roc_curve.py

--- data/masif_ligand/analysis/roc_curve.py
+++ data/masif_ligand/analysis/roc_curve.py
@@ -28,3 +28,61 @@ for i in range(n_classes):
 fpr["micro"], tpr["micro"], _ = roc_curve(y_true.ravel(), y_pred_probs.ravel())
 roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
 
+
+
+# First aggregate all false positive rates
+all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+# Then interpolate all ROC curves at this points
+mean_tpr = np.zeros_like(all_fpr)
+for i in range(n_classes):
+    mean_tpr += np.interp(all_fpr, fpr[i], tpr[i])
+
+# Finally average it and compute AUC
+mean_tpr /= n_classes
+
+fpr["macro"] = all_fpr
+tpr["macro"] = mean_tpr
+roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+
+# Plot all ROC curves
+plt.figure()
+plt.plot(
+    fpr["micro"],
+    tpr["micro"],
+    label="micro-average ROC curve (area = {0:0.2f})".format(roc_auc["micro"]),
+    color="deeppink",
+    linestyle=":",
+    linewidth=4,
+)
+
+plt.plot(
+    fpr["macro"],
+    tpr["macro"],
+    label="macro-average ROC curve (area = {0:0.2f})".format(roc_auc["macro"]),
+    color="navy",
+    linestyle=":",
+    linewidth=4,
+)
+
+colors = cycle(["aqua", "darkorange", "cornflowerblue"])
+for i, color in zip(range(n_classes), colors):
+    plt.plot(
+        fpr[i],
+        tpr[i],
+        color=color,
+        lw=lw,
+        label="ROC curve of class {0} (area = {1:0.2f})".format(i, roc_auc[i]),
+    )
+
+plt.plot([0, 1], [0, 1], "k--", lw=lw)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.title("Some extension of Receiver operating characteristic to multiclass")
+plt.legend(loc="lower right")
+
+plt.savefig('roc_curve.png')
+#plt.show()
