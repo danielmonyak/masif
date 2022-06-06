525ef8116181bfee70f874e522bae1db1b418776
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-06 14:37:04 -0400

    Update train_new.py

--- source/tf2/train_new.py
+++ source/tf2/train_new.py
@@ -7,9 +7,7 @@ from IPython.core.debugger import set_trace
 import importlib
 import sys
 from default_config.masif_opts import masif_opts
-#####
-from prep_data import model
-#####
+from MaSIF_ligand_TF2 import MaSIf_ligand
 from sklearn.metrics import confusion_matrix
 import tensorflow as tf
 
@@ -18,6 +16,14 @@ train_X = np.load(datadir + 'train_X.npy')
 train_y = np.load(datadir + 'train_y.npy')
 val_X = np.load(datadir + 'val_X.npy')
 val_y = np.load(datadir + 'val_X.npy')
+
+model = MaSIF_ligand(
+  params["max_distance"],
+  params["n_classes"],
+  idx_gpu="/gpu:0",
+  feat_mask=params["feat_mask"],
+  costfun=params["costfun"]
+)
 
 num_epochs = 100
 #num_batches = 32
