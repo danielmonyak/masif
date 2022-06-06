6569b2d5e698fc2b2ceeb40647e215f531bfa230
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-06 15:51:19 -0400

    Update train_model.py

--- source/tf2/train_model.py
+++ source/tf2/train_model.py
@@ -44,15 +44,15 @@ saveCheckpoints = tf.keras.callbacks.ModelCheckpoint(
 )
 
 num_epochs = 100
-with strategy.scope():
-  history = model.fit(x = train_X, y = train_y,
-    epochs = num_epochs,
-    validation_data = (val_X, val_y),
-    callbacks = [saveCheckpoints],
-    verbose = 2,
-    use_multiprocessing = True
-  )
-  model.evaluate(x_test,  y_test_encoded, verbose=2)
+#with strategy.scope():
+history = model.fit(x = train_X, y = train_y,
+  epochs = num_epochs,
+  validation_data = (val_X, val_y),
+  callbacks = [saveCheckpoints],
+  verbose = 2,
+  use_multiprocessing = True
+)
+model.evaluate(x_test,  y_test_encoded, verbose=2)
 
 #model.save(modelDir)
 with open(modelDir + '/train_history', 'wb') as file_pi:
