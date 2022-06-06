38fe72601bed0a40703f40bde24e1e34cf13b030
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-06 14:21:06 -0400

    Update train_new.py

--- source/tf2/train_new.py
+++ source/tf2/train_new.py
@@ -2,71 +2,26 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
 
-
 import numpy as np
 from IPython.core.debugger import set_trace
 import importlib
 import sys
 from default_config.masif_opts import masif_opts
 #####
-# Edited by Daniel Monyak
 from prep_data import model
 #####
-from read_ligand_tfrecords import _parse_function
 from sklearn.metrics import confusion_matrix
 import tensorflow as tf
 
-continue_training = True
-
-
-params = masif_opts["ligand"]
-
-
-# Load dataset
-training_data = tf.data.TFRecordDataset(
-    os.path.join(params["tfrecords_dir"], "training_data_sequenceSplit_30.tfrecord")
-)
-validation_data = tf.data.TFRecordDataset(
-    os.path.join(params["tfrecords_dir"], "validation_data_sequenceSplit_30.tfrecord")
-)
-testing_data = tf.data.TFRecordDataset(
-    os.path.join(params["tfrecords_dir"], "testing_data_sequenceSplit_30.tfrecord")
-)
-training_data = training_data.map(_parse_function)
-validation_data = validation_data.map(_parse_function)
-testing_data = testing_data.map(_parse_function)
-
-training_data = iter(training_data)
-validation_data = iter(validation_data)
-testing_data = iter(testing_data)
-
-
-
-out_dir = params["model_dir"]
-output_model = out_dir + "model"
-if not os.path.exists(params["model_dir"]):
-    os.makedirs(params["model_dir"])
-
-    
-'''
-# Create Model
-model = MaSIF_ligand(
-  params["max_distance"],
-  params["n_classes"],
-  idx_gpu="/gpu:0",
-  feat_mask=params["feat_mask"],
-  costfun=params["costfun"]
-)
-
-model.compile(optimizer = model.opt,
-  loss = model.loss_fn,
-  metrics=['accuracy']
-)
-'''
+datadir = 'datasets/'
+train_X = np.load(datadir + 'train_X.npy')
+train_y = np.load(datadir + 'train_y.npy')
+val_X = np.load(datadir + 'val_X.npy')
+val_y = np.load(datadir + 'val_X.npy')
 
 num_epochs = 100
 #num_batches = 32
-model.fit(x = train_X_formatted, y = train_y_formatted,
+model.fit(x = train_X, y = train_y,
           epochs = num_epochs,
-          validation_data = (val_X_formatted, val_y_formatted)
+          validation_data = (val_X, val_y)
          )
