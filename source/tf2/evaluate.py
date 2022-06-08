95ee159b4cb8922fd5f66191d4ebaf7e15930d44
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-08 09:55:09 -0400

    Create evaluate.py

---
+++ source/tf2/evaluate.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+import numpy as np
+
+model = tf.keras.models.load_model('kerasModel/savedModel')
+
+datadir = 'datasets/'
+train_X = np.load(datadir + 'train_X.npy')
+train_y = np.load(datadir + 'train_y.npy')
+val_X = np.load(datadir + 'val_X.npy')
+val_y = np.load(datadir + 'val_y.npy')
+test_X = np.load(datadir + 'test_X.npy')
+test_y = np.load(datadir + 'test_y.npy')
+
+gpus = tf.config.experimental.list_logical_devices('GPU')
+gpus_str = [g.name for g in gpus]
+strategy = tf.distribute.MirroredStrategy(gpus_str[1:])
+
+with strategy.scope():
+  train_res = model.evaluate(train_X, train_y, use_multiprocessing=True)
+  val_res = model.evaluate(val_X, val_y, use_multiprocessing=True)
+  test_res = model.evaluate(test_X, test_y, use_multiprocessing=True)
