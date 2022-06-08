e4b672945993e416a596ff7a3a69d8f3f38320fd
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-08 17:20:33 -0400

    Update test.py

--- source/tf2/test.py
+++ source/tf2/test.py
@@ -15,25 +15,17 @@ test_X = tf.RaggedTensor.from_tensor(test_X_raw, padding=defaultCode)
 print(test_X[0].shape[0])
 
 prodFunc = lambda a,b : a*b
+makeRagged = lambda tsr: tf.RaggedTensor.from_tensor(tsr, ragged_rank = 2)
 def func(row):
 	n_pockets = int(row.shape[0]/8)
 	bigShape = [int(n_pockets/200), 200, 5]
 	smallShape = [int(n_pockets/200), 200, 1]
 	idx = int(functools.reduce(prodFunc, bigShape))
-	#print('idx:', idx)
-	#print('row.shape[0]:', row.shape[0])
-	#print('bigShape:', bigShape)
-	#print('smallShape:', smallShape)
-	def makeRagged(tsr):
-		return tf.RaggedTensor.from_tensor(tsr, ragged_rank = 2)
-	feat = makeRagged(tf.reshape(row[:idx], bigShape))
+	feat = tf.reshape(row[:idx], bigShape)
 	rest = tf.reshape(row[idx:], [3] + smallShape)
-	return [feat, makeRagged(rest[0]), makeRagged(rest[1]), makeRagged(rest[2])]
-	#return makeRagged(rest[0])
+	return [makeRagged(tsr) for tsr in [feat, rest[0], rest[1], rest[2]]
 
 featType = tf.RaggedTensorSpec(shape=[None, 200, 5], dtype=tf.float32)
 restType = tf.RaggedTensorSpec(shape=[None, 200, 1], dtype=tf.float32)
-input_feat_full = tf.map_fn(fn=func, elems = test_X, fn_output_signature = [featType, restType, restType, restType])
-#input_feat_full = tf.map_fn(fn=func, elems = test_X, fn_output_signature = restType)
-
+data_list = tf.map_fn(fn=func, elems = test_X, fn_output_signature = [featType, restType, restType, restType])
 
