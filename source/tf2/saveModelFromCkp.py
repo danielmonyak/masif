2ca28cd97f6b5ce6b1e94f0b278a99d866e2975e
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-08 09:45:54 -0400

    Update saveModelFromCkp.py

--- source/tf2/saveModelFromCkp.py
+++ source/tf2/saveModelFromCkp.py
@@ -4,6 +4,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 from default_config.masif_opts import masif_opts
 from MaSIF_ligand_TF2 import MaSIF_ligand
 import tensorflow as tf
+import numpy as np
 
 params = masif_opts["ligand"]
 
@@ -20,9 +21,15 @@ model.compile(optimizer = model.opt,
   loss = model.loss_fn,
   metrics=['accuracy']
 )
-'''
-model.build([model.bigLen + model.smallLen * 3])
 
+import numpy as np
+datadir = 'datasets/'
+train_X = np.load(datadir + 'train_X.npy')
+train_y = np.load(datadir + 'train_y.npy')
+_ = model.predict(train_X[:1])
+
+#model.build([None, model.bigLen + model.smallLen * 3])
+'''
 model.load_weights(ckpPath)
 model.save(modelPath)
 '''
