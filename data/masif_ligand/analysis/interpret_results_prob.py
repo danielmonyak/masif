882dc6d8bdc92b2f9aff8759e79173dc2076effe
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-09 12:55:43 -0400

    Update interpret_results_prob.py

--- data/masif_ligand/analysis/interpret_results_prob.py
+++ data/masif_ligand/analysis/interpret_results_prob.py
@@ -24,22 +24,15 @@ for pdb in saved_pdbs:
     y_true_list.append(labels[0])
     
     logits_softmax = np.load(test_set_out_dir + "{}_logits.npy".format(pdb)).astype(float)
-    logits_softmax = logits_softmax.reshape([logits_softmax.shape[0], logits_softmax.shape[1], n_ligands])
-    avg_softmax = logits_softmax.mean(axis = 1).mean(axis = 0)
+    logits_softmax = np.squeeze(logits_softmax, axis = (2,3))
+    avg_softmax = np.mean(logits_softmax, axis = (0,1))
+    
     y_pred_list.append(avg_softmax)
 
 y_true = np.array(y_true_list)
 y_pred_probs = np.vstack(y_pred_list)
 y_pred = y_pred_probs.argmax(axis = 1)
 
-
 np.savetxt('y_true.txt', y_true)
 np.savetxt('y_pred_probs.txt', y_pred_probs)
 np.savetxt('y_pred.txt', y_pred)
-
-
-'''
-y_true = np.array(y_true_list).reshape([-1, 1]) 
-enc = OneHotEncoder(categories = [np.arange(n_ligands)])
-y_true_one_hot = enc.fit_transform(y_true).toarray()
-'''
