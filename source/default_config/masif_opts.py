c860345a063438c04f252f73ab29290ca401c4c8
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-10 10:22:15 -0400

    Update masif_opts.py

--- source/default_config/masif_opts.py
+++ source/default_config/masif_opts.py
@@ -83,4 +83,4 @@ masif_opts["ligand"]["costfun"] = "dprime"
 masif_opts["ligand"]["model_dir"] = "nn_models/all_feat/"
 masif_opts["ligand"]["test_set_out_dir"] = "test_set_predictions/"
 masif_opts['ligand']['minPockets'] = 32
-masif_opts['ligand']['defaultCode'] = 123.456789
+masif_opts['ligand']['defaultCode'] = -1234567
