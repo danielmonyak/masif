1a629f9aa9a87ff84a76be39a19bfbee6986b9d2
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-10 13:49:02 -0400

    Update prep_data.py

--- source/ligand_site/prep_data.py
+++ source/ligand_site/prep_data.py
@@ -36,52 +36,52 @@ def compile_and_save(feed_list, y_list, j):
 dataset_list = {'train' : "training_data_sequenceSplit_30.tfrecord", 'val' : "validation_data_sequenceSplit_30.tfrecord", 'test' : "testing_data_sequenceSplit_30.tfrecord"}
 
 gpus = tf.config.experimental.list_logical_devices('GPU')
-#dev = '/GPU:2'
-dev = gpus[1].name
+dev = '/GPU:1'
+#dev = gpus[1].name
 #tf.config.experimental.set_memory_growth(gpus, True)
 #gpus_str = [g.name for g in gpus]
 #strategy = tf.distribute.MirroredStrategy(gpus_str[1:])
 
 #with strategy.scope():
-#with tf.device(dev):
-for dataset in dataset_list.keys():
+with tf.device(dev):
+    for dataset in dataset_list.keys():
 
-    print('\n' + dataset)
-    i = 0
-    j = 0
+        print('\n' + dataset)
+        i = 0
+        j = 0
 
-    feed_list = []
-    y_list = []
+        feed_list = []
+        y_list = []
 
-    temp_data = tf.data.TFRecordDataset(os.path.join(params["tfrecords_dir"], dataset_list[dataset])).map(_parse_function)
-    for data_element in temp_data:
-        print('{} record {}'.format(dataset, i))
+        temp_data = tf.data.TFRecordDataset(os.path.join(params["tfrecords_dir"], dataset_list[dataset])).map(_parse_function)
+        for data_element in temp_data:
+            print('{} record {}'.format(dataset, i))
 
-        labels = data_element[4]
-        n_ligands = labels.shape[1]
-        if n_ligands > 1:
-            print('More than one ligand, check this out...')
-            continue
+            labels = data_element[4]
+            n_ligands = labels.shape[1]
+            if n_ligands > 1:
+                print('More than one ligand, check this out...')
+                continue
 
-        #one_hot_labels = tf.one_hot(tf.squeeze(labels) - 1, n_classes)
-        y_list.append(tf.squeeze(labels))
+            #one_hot_labels = tf.one_hot(tf.squeeze(labels) - 1, n_classes)
+            y_list.append(tf.squeeze(labels))
 
-        feed_dict = {
-            'input_feat' : data_element[0],
-            'rho_coords' : np.expand_dims(data_element[1], -1),
-            'theta_coords' : np.expand_dims(data_element[2], -1),
-            'mask' : data_element[3],
-        }
-        feed_list.append(feed_dict)
+            feed_dict = {
+                'input_feat' : data_element[0],
+                'rho_coords' : np.expand_dims(data_element[1], -1),
+                'theta_coords' : np.expand_dims(data_element[2], -1),
+                'mask' : data_element[3],
+            }
+            feed_list.append(feed_dict)
 
-        i += 1
+            i += 1
 
-        if i % 100 == 0:
-            #with tf.device(dev):
-            compile_and_save(feed_list, y_list, j)
-            feed_list = []
-            y_list = []
-            j += 1
+            if i % 100 == 0:
+                #with tf.device(dev):
+                compile_and_save(feed_list, y_list, j)
+                feed_list = []
+                y_list = []
+                j += 1
 
 
 print('Finished!')
