f065bc3127025c6c292d89a2040f2c04e1deac19
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-10 14:37:14 -0400

    Update compile_data.py

--- source/ligand_site/compile_data.py
+++ source/ligand_site/compile_data.py
@@ -8,14 +8,21 @@ from default_config.masif_opts import masif_opts
 from tf2.read_ligand_tfrecords import _parse_function
 import tensorflow as tf
 
+params = masif_opts["ligand"]
+defaultCode = params['defaultCode']
+
 datadir = '/data02/daniel/masif/datasets/ligand_site'
 genPath = os.path.join(datadir, '{}_{}_{}.npy')
 
 dev = '/GPU:1'
 with tf.device(dev):
   j = 4
+  print('one')
   train_X_raw = np.load(genPath.format('train', 'X', j))
+  print('two')
   train_y_raw = np.load(genPath.format('train', 'y', j))
+  print('three')
   train_X = tf.RaggedTensor.from_tensor(train_X_raw, padding=defaultCode)
-  train_y_raw = np.load(genPath.format('train', 'y', j))
+  print('four')
+  train_y = tf.RaggedTensor.from_tensor(train_y_raw, padding=defaultCode)
   
