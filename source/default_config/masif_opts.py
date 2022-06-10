2c2a6c3f4ccabbae7d75b2fdbf96c41cc45a416d
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-10 12:37:45 -0400

    Update masif_opts.py

--- source/default_config/masif_opts.py
+++ source/default_config/masif_opts.py
@@ -56,7 +56,7 @@ masif_opts["site"]["n_conv_layers"] = 3
 masif_opts["site"]["max_distance"] = 9.0  # Radius for the neural network.
 masif_opts["site"][
     "masif_precomputation_dir"
-] = "/data02/daniel/masif/sitedata_preparation/04a-precomputation_9A/precomputation/"
+] = "/data02/daniel/masif/site/data_preparation/04a-precomputation_9A/precomputation/"
 masif_opts["site"]["range_val_samples"] = 0.9  # 0.9 to 1.0
 masif_opts["site"]["model_dir"] = "nn_models/all_feat_3l/model_data/"
 masif_opts["site"]["out_pred_dir"] = "output/all_feat_3l/pred_data/"
