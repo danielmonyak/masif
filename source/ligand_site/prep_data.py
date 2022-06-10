f4c0b8bc4c1a25848b589dec4cb3491e37c0da32
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-10 11:06:59 -0400

    Update prep_data.py

--- source/ligand_site/prep_data.py
+++ source/ligand_site/prep_data.py
@@ -38,7 +38,7 @@ dataset_list = {'train' : "training_data_sequenceSplit_30.tfrecord", 'val' : "va
 gpus = tf.config.experimental.list_logical_devices('GPU')
 #dev = '/GPU:2'
 dev = gpus[1].name
-tf.config.experimental.set_memory_growth(dev, True)
+tf.config.experimental.set_memory_growth(gpus, True)
 #gpus_str = [g.name for g in gpus]
 #strategy = tf.distribute.MirroredStrategy(gpus_str[1:])
 
