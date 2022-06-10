5ad9085e02ff88058e2d1946380b7c82d4f646f8
danielmonyak <daniel.monyak@arvinas.com>
2022-06-10 16:30:17 +0000

    update

---
+++ data/masif_site/schedule_data_prepare.sh
@@ -0,0 +1,31 @@
+out=output_files
+err=error_files
+if [ ! -d $out ]; then mkdir $out; fi
+if [ ! -d $err ]; then mkdir $err; fi
+
+batchSize=8
+sleep_time=60
+
+i=0
+unset running
+while read p; do
+	if [ $(( i % $batchSize )) -eq 0 ]; then
+		for pid in ${running[@]}; do
+			while ps -o pid ax | grep -q $pid; do
+				sleep $sleep_time
+			done
+		done
+		running=()
+	fi
+	FIELD1=$(echo $p| cut -d" " -f1)
+        PDBID=$(echo $FIELD1| cut -d"_" -f1)
+        CHAIN1=$(echo $FIELD1| cut -d"_" -f2)
+        CHAIN2=$(echo $FIELD1| cut -d"_" -f3)
+        ./data_prepare_one.sh $PDBID\_$CHAIN1\_$CHAIN2 > $out/$p.out 2>$err/$p.err &
+	disown -h $!
+	running+=($!)
+	i=$((i+1))
+done < lists/masif_site_only.txt
+#done < lists/full_list.txt
+
+echo Finished!
