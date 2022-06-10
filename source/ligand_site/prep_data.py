9bf6e32d7de6c634c182872f76a607ed59fe31d2
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-10 15:38:28 -0400

    Update prep_data.py

--- source/ligand_site/prep_data.py
+++ source/ligand_site/prep_data.py
@@ -29,31 +29,32 @@ def helper(feed_dict):
     flat_list = list(map(helperInner, key_list))
     return tf.concat(flat_list, axis = 0)
 
-def compile_and_save(feed_list, y_list, j):
+def compile_and_save(feed_list, y_list, dataset, j):
     tsr_list = list(map(helper, feed_list))
     X = tf.ragged.stack(tsr_list).to_tensor(default_value = defaultCode)
     y = tf.ragged.stack(y_list).to_tensor(default_value = defaultCode)
     np.save(genOutPath.format(dataset, 'X_{}'.format(j)), X)
     np.save(genOutPath.format(dataset, 'y_{}'.format(j)), y)
 
-#dataset_list = {'train' : "training_data_sequenceSplit_30.tfrecord", 'val' : "validation_data_sequenceSplit_30.tfrecord", 'test' : "testing_data_sequenceSplit_30.tfrecord"}
-dataset_list = {'train' : "training_data_sequenceSplit_30.tfrecord"}
+dataset_list = {'train' : "training_data_sequenceSplit_30.tfrecord", 'val' : "validation_data_sequenceSplit_30.tfrecord", 'test' : "testing_data_sequenceSplit_30.tfrecord"}
+#dataset_list = {'train' : "training_data_sequenceSplit_30.tfrecord"}
 
-#gpus = tf.config.experimental.list_logical_devices('GPU')[1:]
+gpus = tf.config.experimental.list_logical_devices('GPU')[1:]
 dev = '/GPU:1'
 #tf.config.experimental.set_memory_growth(gpus, True)
-#gpus_str = [g.name for g in gpus]
-#strategy = tf.distribute.MirroredStrategy(gpus_str[1:])
+gpus_str = [g.name for g in gpus]
+strategy = tf.distribute.MirroredStrategy(gpus_str[1:])
 
-#with strategy.scope():
-with tf.device(dev):
+with strategy.scope():
+    #with tf.device(dev):
     for dataset in dataset_list.keys():
         i = 0
-        j = lastEpoch
-
+        #j = lastEpoch
+        j = 0
+        
         feed_list = []
         y_list = []
-
+        
         temp_data = tf.data.TFRecordDataset(os.path.join(params["tfrecords_dir"], dataset_list[dataset])).map(_parse_function)
         for data_element in temp_data:
             if i < j*epochSize:
@@ -62,31 +63,44 @@ with tf.device(dev):
             
             print('{} record {}'.format(dataset, i))
 
-            labels = data_element[4]
-            n_ligands = labels.shape[1]
+            labels_raw = data_element[4]
+            n_ligands = labels_raw.shape[1]
             if n_ligands > 1:
                 print('More than one ligand, check this out...')
                 continue
-
+            
+            labels = tf.squeeze(labels_raw)
+            y_list.append(labels)
+            
+            pocket_points = tf.squeeze(tf.where(labels != 0))
+            npoints = pocket_points.shape[0]
+            if npoints < minPockets:
+                continue
+            
+            pocket_empties = tf.squeeze(tf.where(labels == 0))
+            empties_sample = tf.random.shuffle(pocket_empties)[:npoints*4]
+            
+            sample = tf.concat([pocket_points, empties_sample])
+            
             #one_hot_labels = tf.one_hot(tf.squeeze(labels) - 1, n_classes)
-            y_list.append(tf.squeeze(labels))
-
+            
             feed_dict = {
-                'input_feat' : data_element[0],
-                'rho_coords' : np.expand_dims(data_element[1], -1),
-                'theta_coords' : np.expand_dims(data_element[2], -1),
-                'mask' : data_element[3],
+                'input_feat' : tf.gather(data_element[0], sample),
+                'rho_coords' : tf.gather(tf.expand_dims(data_element[1], -1), sample),
+                'theta_coords' : tf.gather(tf.expand_dims(data_element[2], -1), sample),
+                'mask' : tf.gather(data_element[3], sample)
             }
             feed_list.append(feed_dict)
-
+            
             i += 1
-
-            if i % epochSize == 0:
+            
+            '''if i % epochSize == 0:
                 #with tf.device(dev):
                 compile_and_save(feed_list, y_list, j)
                 feed_list = []
                 y_list = []
-                j += 1
-
+                j += 1'''
+        
+        compile_and_save(feed_list, y_list, dataset, j)
 
 print('Finished!')
