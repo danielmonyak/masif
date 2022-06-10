d11b68e86b7cf52ddb93f066cb76e433a3ef2260
danielmonyak <daniel.monyak@arvinas.com>
2022-06-10 16:30:17 +0000

    update

--- data/masif_site/data_prepare_one.sh
+++ data/masif_site/data_prepare_one.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
+
+tf_env=venv_latest
+env_path=/apps01/anaconda3/envs/$tf_env
+
+export APBS_BIN=$HOME/software/APBS-3.4.1.Linux/bin/apbs
+export MULTIVALUE_BIN=$HOME/software/APBS-3.4.1.Linux/share/apbs/tools/bin/multivalue
+export PDB2PQR_BIN=$env_path/bin/pdb2pqr
+export PATH=$HOME/software/reduce/reduce_src:$PATH
+export REDUCE_HET_DICT=$HOME/software/reduce/reduce_wwPDB_het_dict.txt
+export PYMESH_PATH=$HOME/software/PyMesh
+export MSMS_BIN=$env_path/bin/msms
+export PDB2XYZRN=$env_path/bin/pdb_to_xyzrn
+
+source /apps01/anaconda3/etc/profile.d/conda.sh
+conda activate $tf_env
+
+
+
 masif_root=$(git rev-parse --show-toplevel)
 masif_source=$masif_root/source/
 masif_matlab=$masif_root/source/matlab_libs/
 export PYTHONPATH=$PYTHONPATH:$masif_source
-export masif_matlab
 if [ "$1" == "--file" ]
 then
 	echo "Running masif site on $2"
@@ -31,3 +48,6 @@ else
     python -W ignore $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN2
 fi
 python $masif_source/data_preparation/04-masif_precompute.py masif_site $PPI_PAIR_ID
+
+conda deactivate
+echo Finished
