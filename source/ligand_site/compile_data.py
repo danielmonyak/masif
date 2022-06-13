1ec8cf922d575a9eeaf3c6e3a6dc2970324a7000
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-13 14:40:32 -0400

    Update compile_data.py

--- source/ligand_site/compile_data.py
+++ source/ligand_site/compile_data.py
@@ -12,31 +12,19 @@ params = masif_opts["ligand"]
 defaultCode = params['defaultCode']
 
 datadirLS = '/data02/daniel/masif/datasets/ligand_site'
-#genPath = os.path.join(datadir, '{}_{}_{}.npy')
-datadirTF2 = '/data02/daniel/masif/datasets/tf2'
+genPath = os.path.join(datadir, '{}_{}_{}.npy')
+#datadirTF2 = '/data02/daniel/masif/datasets/tf2'
 #genPath = os.path.join(datadir, '{}_{}.npy')
 
 dev = '/GPU:1'
-train_j = range(4)
-'''with tf.device(dev):
-  j = 0
-  train_X_list = []
-  for j in range(2):
-    print(j)
-    #train_X_temp = np.load(os.path.join(datadirLS, 'train_X_{}.npy'.format(j)))
-    train_X_temp = np.load(os.path.join(datadirTF2, 'train_X.npy'))
-    print('one')
-    train_X_temp = tf.RaggedTensor.from_tensor(train_X_temp, padding=defaultCode)
-    print('two')
-    train_X_list.append(train_X_temp)
-  print('three')
-  train_X = tf.concatenate(train_X_list)
-  print('four')
-  #train_y_raw = np.load(genPath.format('train', 'y'))
-  #train_y = tf.RaggedTensor.from_tensor(train_y_raw, padding=defaultCode)'''
+train_j = range(10)
 
 train_X_temp = np.load(os.path.join(datadirLS, 'train_X_{}.npy'.format(0)))
-#train_X_temp = np.load(os.path.join(datadirTF2, 'train_X.npy'))
 #train_X_temp = tf.RaggedTensor.from_tensor(train_X_temp, padding=defaultCode)
-  
-print(train_X_temp.shape)
+
+train_X_list = []
+for j in train_j:
+  print(j)
+  train_X_list.append(np.load(os.path.join(datadirLS, 'train_X_{}.npy'.format(0))))
+
+train_X = tf.stack(train_X_list)
