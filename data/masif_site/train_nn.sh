66a09ffbb9d701e06d57ad1553949ee923958d47
danielmonyak <daniel.monyak@arvinas.com>
2022-06-13 15:43:55 +0000

    update

--- data/masif_site/train_nn.sh
+++ data/masif_site/train_nn.sh
@@ -5,6 +5,7 @@ masif_root=$(git rev-parse --show-toplevel)
 masif_source=$masif_root/source/
 masif_data=$masif_root/data/
 export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_site/
-python3 $masif_source/masif_site/masif_site_train.py $1
 
-echo Finished!
+python -u $masif_source/masif_site/masif_site_train.py $1 > train_nn.out 2>train_nn.err &
+disown -h $!
+echo $! > train_nn_pid.txt
