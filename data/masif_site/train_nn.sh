dabef1844f867a92bda37f387803823cbe5ae575
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-13 11:35:48 -0400

    Update train_nn.sh

--- data/masif_site/train_nn.sh
+++ data/masif_site/train_nn.sh
@@ -1,5 +1,10 @@
+source /apps01/anaconda3/etc/profile.d/conda.sh
+conda activate venv_tf
+
 masif_root=$(git rev-parse --show-toplevel)
 masif_source=$masif_root/source/
 masif_data=$masif_root/data/
 export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_site/
 python3 $masif_source/masif_site/masif_site_train.py $1
+
+echo Finished!
