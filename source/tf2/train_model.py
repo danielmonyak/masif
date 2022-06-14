20433938c647df8f8daad328503d3a22277d8c86
danielmonyak <daniel.monyak@arvinas.com>
2022-06-14 22:14:28 +0000

    update

--- source/tf2/train_model.py
+++ source/tf2/train_model.py
@@ -26,6 +26,8 @@ train_y = np.load(genPath.format('train', 'y'))
 val_X = np.load(genPath.format('val', 'X'))
 val_y = np.load(genPath.format('val', 'y'))
 
+defaultCode = -1234567
+
 train_X = tf.RaggedTensor.from_tensor(train_X, padding=defaultCode)
 val_X = tf.RaggedTensor.from_tensor(val_X, padding=defaultCode)
 
