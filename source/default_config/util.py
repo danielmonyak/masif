5d118e409e14413c9a02d43accbf47c2f4d401c5
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-14 14:54:09 -0400

    Create util.py

---
+++ source/default_config/util.py
@@ -0,0 +1,10 @@
+from default_config.masif_opts import masif_opts
+params = masif_opts["ligand"]
+minPockets = params['minPockets']
+
+inputFeatSpec = tf.RaggedTensorSpec(shape=[None, 200, 5], dtype=tf.float32)
+restSpec = tf.RaggedTensorSpec(shape=[None, 200, 1], dtype=tf.float32)
+sampleSpec = tf.TensorSpec([minPockets], dtype=tf.int32)
+
+prodFunc = lambda a,b : a*b
+makeRagged = lambda tsr: tf.RaggedTensor.from_tensor(tsr, ragged_rank = 2)
