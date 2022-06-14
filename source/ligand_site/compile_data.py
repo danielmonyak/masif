6c24b88868730151b6289eb31810b1c2e37220da
danielmonyak <daniel.monyak@arvinas.com>
2022-06-14 21:05:35 +0000

    update

--- source/ligand_site/compile_data.py
+++ source/ligand_site/compile_data.py
@@ -10,17 +10,14 @@ import tensorflow as tf
 
 params = masif_opts["ligand"]
 
-datadir = '/data02/daniel/masif/datasets/ligand_site'
+datadir = '/data02/daniel/masif/datasets/ligand_site/split'
 genPathIn = os.path.join(datadir, '{}_{}_{}.npy')
 
-outDir = '/data02/daniel/masif/datasets/ligand_site/complete'
+outDir = '/data02/daniel/masif/datasets/ligand_site'
 genPathOut = os.path.join(outDir, '{}_{}.npy')
 
 dev = '/GPU:1'
 train_j = range(10)
-
-#defaultCode = params['defaultCode']
-#train_X_temp = tf.RaggedTensor.from_tensor(train_X_temp, padding=defaultCode)
 
 numFiles_dict = {'train' : 10, 'val' : 2, 'test' : 3}
 
@@ -34,5 +31,5 @@ for dataset in numFiles_dict.keys():
     y_list.append(np.load(genPathIn.format(dataset, 'y', j)))
   X = tf.concat(X_list, axis=0)
   y = tf.concat(y_list, axis=0)
-  np.save(genPathOut.format(dataset, 'X'), X) 
-  np.save(genPathOut.format(dataset, 'y'), y) 
+  np.save(genPathOut.format(dataset, 'X'), X)
+  np.save(genPathOut.format(dataset, 'y'), y)
