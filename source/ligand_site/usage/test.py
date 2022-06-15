4994973ebb667100c91ed78d3548296c046745ba
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-15 18:58:02 -0400

    Update test.py

--- source/ligand_site/usage/test.py
+++ source/ligand_site/usage/test.py
@@ -8,13 +8,6 @@ pdb = '1C75_A_'
 precom_dir = '/data02/daniel/masif/data_preparation/04a-precomputation_12A/precomputation'
 pred = Predictor(ligand_model_path, ligand_site_ckp_path)
 
-self= pred
 pdb_dir = os.path.join(precom_dir, pdb)
-X = pred.getData(pdb_dir)
 
-'''
-i = 0
-sample = tf.expand_dims(tf.range(minPockets * i, minPockets * (i+1)), axis=0)
-
-a = pred.ligand_site_model(X, sample)
-'''
+ret = pred(pdb_dir)
