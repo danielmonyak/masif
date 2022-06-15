346c6cffc34b9feb54fa13a29fb6042f02d905a4
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-15 11:55:35 -0400

    Update MaSIF_ligand_site.py

--- source/ligand_site/MaSIF_ligand_site.py
+++ source/ligand_site/MaSIF_ligand_site.py
@@ -218,8 +218,7 @@ class ConvLayer(layers.Layer):
                                           fn_output_signature = [inputFeatSpec, restSpec, restSpec, restSpec])
         return [tf.gather(params = data, indices = sample, axis = 1, batch_dims = 1).to_tensor() for data in data_list]
     
-    def call(self, x, **kwargs):
-        sample = kwargs['sample']
+    def call(self, x, sample = None):
         input_feat, rho_coords, theta_coords, mask = self.unpack_x(x, sample)
         
         self.global_desc_1 = []
