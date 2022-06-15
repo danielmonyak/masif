e021b98b518b076c532bd0193dd74ba2bba256d5
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-15 17:27:24 -0400

    Update test.py

--- source/ligand_site/usage/test.py
+++ source/ligand_site/usage/test.py
@@ -1,13 +1,20 @@
 from predictor import *
 
 ligand_model_path = 'kerasModel/savedModel'
-ligand_site_model_path = '../kerasModel/savedModel'
-precom_dir = '/data02/daniel/masif/data_preparation/04a-precomputation_12A/precomputation/3O7W_A_'
-pred = Predictor(ligand_model_path, ligand_site_model_path)
+#ligand_site_model_path = '../kerasModel/savedModel'
+ligand_site_ckp_path = '../kerasModel/ckp'
 
-X = pred.getData(precom_dir)
+pdb = '1C75_A_'
+precom_dir = '/data02/daniel/masif/data_preparation/04a-precomputation_12A/precomputation'
+pred = Predictor(ligand_model_path, ligand_site_ckp_path)
+
+self= pred
+pdb_dir = os.path.join(precom_dir, pdb)
+X = pred.getData(pdb_dir)
+
+'''
 i = 0
-sample = tf.range(minPockets * i, minPockets * (i+1))
+sample = tf.expand_dims(tf.range(minPockets * i, minPockets * (i+1)), axis=0)
 
 a = pred.ligand_site_model(X, sample)
-#a = pred.ligand_site_model.myConvLayer(X, sample = sample)
+'''
