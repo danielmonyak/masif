8cec41a5390756760b63dfdc98a82c41fbac32f8
danielmonyak <daniel.monyak@arvinas.com>
2022-06-17 06:00:55 +0000

    update

--- source/ligand_site/usage/test.py
+++ source/ligand_site/usage/test.py
@@ -1,12 +1,5 @@
 import binding
 
-
-precom_dir = '/data02/daniel/masif/data_preparation/04a-precomputation_12A/precomputation'
-ligand_model_path = '/home/daniel.monyak/software/masif/source/tf2/kerasModel/savedModel'
-ligand_site_ckp_path = '/home/daniel.monyak/software/masif/source/ligand_site/kerasModel/ckp'
-
-pred = Predictor(ligand_model_path, ligand_site_ckp_path)
-
-#pdb='3AYI_AB_'
-pdb='1C75_A_'
-binding.predict(pred, pdb)
+pdb='3AYI_AB_'
+#pdb='1C75_A_'
+binding.predict(pdb)
---
+++ source/ligand_site/usage/test.sh
@@ -0,0 +1,2 @@
+python test.py > test.out 2> test.err &
+echo $! > test.pid
