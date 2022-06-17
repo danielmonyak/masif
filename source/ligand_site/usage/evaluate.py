e6aa2aae7eaabc0a9b672c947700ceb2df3d1192
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-17 17:46:31 -0400

    Update evaluate.py

--- source/ligand_site/usage/evaluate.py
+++ source/ligand_site/usage/evaluate.py
@@ -6,6 +6,9 @@ from scipy import spatial
 from default_config.util import *
 from predictor import Predictor
 
+continue_running = True
+outdir = 'results'
+
 params = masif_opts['ligand']
 ligand_list = params['ligand_list']
 
@@ -23,80 +26,74 @@ fileName = 'test_pdbs_sequence.npy'
 test_list = np.load(os.path.join(listDir, fileName)).astype(str)
 test_list = np.char.add(test_list, '_')
 
-f1_scores = []
-lig_true = []
-lig_pred = []
-
-outdir = 'results'
 if not os.path.exists(outdir):
     os.mkdir(outdir)
 
 pdb_file = os.path.join(outdir, 'pdbs.txt')
-
 recall_file = os.path.join(outdir, 'recall.txt')
 precision_file = os.path.join(outdir, 'precision.txt')
-
-#f1_file = os.path.join(outdir, 'f1.txt')
-
 lig_true_file = os.path.join(outdir, 'lig_true.txt')
 lig_pred_file = os.path.join(outdir, 'lig_pred.txt')
 
-for fi in [pdb_file, recall_file, precision_file, lig_true_file, lig_pred_file]:
-    with open(fi, 'w') as f:
-        pass
+if continue_running:
+    pdbs_done = np.loadtxt(pdb_file, dtype=str)
+    pdbs_left = test_list[~np.isin(test_list, pdbs_done)]
+else:
+    pdbs_left = test_list
+    for fi in [pdb_file, recall_file, precision_file, lig_true_file, lig_pred_file]:
+        with open(fi, 'w') as f:
+            pass
 
 n_test = len(test_list)
-for i, pdb in enumerate(test_list):
-    if i == n_test:
-        break
-    
-    print('{} of {} test pdbs running...'.format(i, n_test))
-    pdb_dir = os.path.join(precom_dir, pdb)
-    ligandIdx_pred, pocket_points_pred = pred.predictRaw(pdb_dir)
-    xyz_coords = pred.getXYZCoords(pdb_dir)
-    
-    all_ligand_coords = np.load(
-        os.path.join(
-            ligand_coord_dir, "{}_ligand_coords.npy".format(pdb.split("_")[0])
-        )
-    )
-    all_ligand_types = np.load(
-        os.path.join(
-            ligand_coord_dir, "{}_ligand_types.npy".format(pdb.split("_")[0])
-        )
-    ).astype(str)
 
-    #if len(all_ligand_coords) == 0:
-    #    continue
-    try:
-        ligand_coords = all_ligand_coords[0]
-        tree = spatial.KDTree(xyz_coords)
-        pocket_points_true = tree.query_ball_point(ligand_coords, 3.0)
-        pocket_points_true = list(set([pp for p in pocket_points_true for pp in p]))
-        
-        overlap = np.intersect1d(pocket_points_true, pocket_points_pred)
-        recall = len(overlap)/len(pocket_points_true)
-        precision = len(overlap)/len(pocket_points_pred)
-        #f1 = 2*recall*precision / (recall + precision)
-    
-        ligand_true = all_ligand_types[0]
-        ligandIdx_true = ligand_list.index(ligand_true)
-    except:
-        continue
-    #f1_scores.append(f1)
-    #lig_true.append(ligandIdx_true)
-    #lig_pred.append(ligandIdx_pred)
-    
-    with open(pdb_file, 'a') as f:
-        f.write(str(pdb) + '\n')
-    
-    with open(recall_file, 'a') as f:
-        f.write(str(recall) + '\n')
-    with open(precision_file, 'a') as f:
-        f.write(str(precision) + '\n')
-    
-    with open(lig_true_file, 'a') as f:
-        f.write(str(ligandIdx_true) + '\n')
-    with open(lig_pred_file, 'a') as f:
-        f.write(str(ligandIdx_pred) + '\n')
+with tf.device('/GPU:2'):
+    for i, pdb in enumerate(pdbs_left):
+        if i == n_test:
+            break
 
+        print('{} of {} test pdbs running...'.format(i, n_test))
+        pdb_dir = os.path.join(precom_dir, pdb)
+        ligandIdx_pred, pocket_points_pred = pred.predictRaw(pdb_dir)
+        xyz_coords = pred.getXYZCoords(pdb_dir)
+
+        all_ligand_coords = np.load(
+            os.path.join(
+                ligand_coord_dir, "{}_ligand_coords.npy".format(pdb.split("_")[0])
+            )
+        )
+        all_ligand_types = np.load(
+            os.path.join(
+                ligand_coord_dir, "{}_ligand_types.npy".format(pdb.split("_")[0])
+            )
+        ).astype(str)
+
+        #if len(all_ligand_coords) == 0:
+        #    continue
+        try:
+            ligand_coords = all_ligand_coords[0]
+            tree = spatial.KDTree(xyz_coords)
+            pocket_points_true = tree.query_ball_point(ligand_coords, 3.0)
+            pocket_points_true = list(set([pp for p in pocket_points_true for pp in p]))
+
+            overlap = np.intersect1d(pocket_points_true, pocket_points_pred)
+            recall = len(overlap)/len(pocket_points_true)
+            precision = len(overlap)/len(pocket_points_pred)
+            #f1 = 2*recall*precision / (recall + precision)
+
+            ligand_true = all_ligand_types[0]
+            ligandIdx_true = ligand_list.index(ligand_true)
+        except:
+            continue
+
+        with open(pdb_file, 'a') as f:
+            f.write(str(pdb) + '\n')
+
+        with open(recall_file, 'a') as f:
+            f.write(str(recall) + '\n')
+        with open(precision_file, 'a') as f:
+            f.write(str(precision) + '\n')
+
+        with open(lig_true_file, 'a') as f:
+            f.write(str(ligandIdx_true) + '\n')
+        with open(lig_pred_file, 'a') as f:
+            f.write(str(ligandIdx_pred) + '\n')
