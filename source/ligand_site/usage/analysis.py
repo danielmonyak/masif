0fe8455170d79b9f7d272ff8e5fc8c4eeac75598
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-17 16:47:19 -0400

    Update and rename acc.py to analysis.py

---
+++ source/ligand_site/usage/analysis.py
@@ -0,0 +1,24 @@
+import os
+import numpy as np
+from sklearn.metrics import balanced_accuracy_score, roc_auc_score
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+dir = 'testing'
+
+arr_list = ['lig_true', 'lig_pred', 'recall', 'precision']
+
+input_dict = {}
+for arr in arr_list:
+  path = os.path.join(dir, arr + '.txt')
+  input_dict[arr] = np.loadtxt(path, dtype=float)
+
+df = pd.DataFrame(input_dict)
+
+balanced_acc = balanced_accuracy_score(df['lig_true'], df['lig_pred'])
+print('Balanced accuracy: ', round(balanced_acc, 2))
+
+df['correct'] = df['lig_true'] == df['lig_pred']
+sns.scatterplot(data = df, x = 'precision', y = 'recall', hue = 'correct')
+plt.savefig('plot.png')
