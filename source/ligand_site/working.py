a5635d98647a8ac02a74839e1217418d42cb9a46
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-21 18:49:38 -0400

    Update working.py

--- source/ligand_site/working.py
+++ source/ligand_site/working.py
@@ -111,7 +111,19 @@ ligand_model_path = '/home/daniel.monyak/software/masif/source/tf2/kerasModel/sa
 ligand_site_ckp_path = '/home/daniel.monyak/software/masif/source/ligand_site/kerasModel/ckp'
 pred = Predictor(ligand_model_path, ligand_site_ckp_path)
 
+pdb_dir = os.path.join(precom_dir, pdb)
+pred.loadData(pdb_dir)
+pocket_points_pred = pred.predictPocketPoints()
 
-
+xyz_coords = pred.getXYZCoords(pdb_dir)            
+all_ligand_coords = np.load(
+    os.path.join(
+        ligand_coord_dir, "{}_ligand_coords.npy".format(pdb.split("_")[0])
+    )
+)
+ligand_coords = all_ligand_coords[0]
+tree = spatial.KDTree(xyz_coords)
+pocket_points_true = tree.query_ball_point(ligand_coords, 3.0)
+pocket_points_true = list(set([pp for p in pocket_points_true for pp in p]))
 
 
