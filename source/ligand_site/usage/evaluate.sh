c599a9161ad51ebcb89d82954b9a799044533c2a
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-22 17:38:19 -0400

    Update evaluate.sh

--- source/ligand_site/usage/evaluate.sh
+++ source/ligand_site/usage/evaluate.sh
@@ -1,2 +1,10 @@
+source /apps01/anaconda3/etc/profile.d/conda.sh
+conda activate venv_tf
+
+masif_root=$(git rev-parse --show-toplevel)
+masif_source=$masif_root/source
+export PYTHONPATH=$PYTHONPATH:$masif_source
+
 python evaluate.py > evaluate.out 2> evaluate.err &
 echo $! > evaluate.pid
+disown -h $!
