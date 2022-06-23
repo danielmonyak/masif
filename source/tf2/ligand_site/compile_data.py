da1ce25f874e655d1af279393127fb48a7ac1391
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-23 10:05:23 -0400

    Update compile_data.py

--- source/tf2/ligand_site/compile_data.py
+++ source/tf2/ligand_site/compile_data.py
@@ -17,26 +17,23 @@ outDir = '/data02/daniel/masif/datasets/tf2/ligand_site'
 genPathOut = os.path.join(outDir, '{}_{}.npy')
 
 dev = '/GPU:1'
-train_j = range(10)
 
-#numFiles_dict = {'train' : 10, 'val' : 2, 'test' : 3}
+with tf.device(dev):
+  for dataset in ['train', 'val', 'test']:
+    temp_X_files = all_files[np.char.startswith(all_files, dataset + '_X')]
 
-with tf.device('/GPU:1'):
-for dataset in ['train', 'val', 'test']:
-  temp_X_files = all_files[np.char.startswith(all_files, dataset + '_X')]
-  
-  print(dataset)
-  X_list = []
-  y_list = []
-  for j, X_file in enumerate(temp_X_files):
-    print(j)
-    y_file = X_file.replace('X', 'y')
-    
-    X_list.append(np.load(os.path.join(datadir, X_file)))
-    y_list.append(np.load(os.path.join(datadir, y_file)))
-  X = tf.concat(X_list, axis=0)
-  y = tf.concat(y_list, axis=0)
-  np.save(genPathOut.format(dataset, 'X'), X)
-  np.save(genPathOut.format(dataset, 'y'), y)
+    print(dataset)
+    X_list = []
+    y_list = []
+    for j, X_file in enumerate(temp_X_files):
+      print(j)
+      y_file = X_file.replace('X', 'y')
+
+      X_list.append(np.load(os.path.join(datadir, X_file)))
+      y_list.append(np.load(os.path.join(datadir, y_file)))
+    X = tf.concat(X_list, axis=0)
+    y = tf.concat(y_list, axis=0)
+    np.save(genPathOut.format(dataset, 'X'), X)
+    np.save(genPathOut.format(dataset, 'y'), y)
 
 print('Finished!')
