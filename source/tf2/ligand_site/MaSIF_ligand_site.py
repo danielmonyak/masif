1e113e885ae9890c749cdb525bc3998edadbd908
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-24 11:31:15 -0400

    Update MaSIF_ligand_site.py

--- source/tf2/ligand_site/MaSIF_ligand_site.py
+++ source/tf2/ligand_site/MaSIF_ligand_site.py
@@ -216,7 +216,7 @@ class ConvLayer(layers.Layer):
                         trainable = True
                     )
                 )
-            '''
+            
             FC1_W = tf.Variable(
                 initial_value=w_init(shape=(self.n_thetas * self.n_rhos * self.n_feat, self.n_thetas * self.n_rhos), dtype="float32"),
                 trainable=True)
@@ -227,7 +227,7 @@ class ConvLayer(layers.Layer):
                 initial_value=w_init(shape=(self.n_thetas * self.n_rhos, self.n_feat), dtype="float32"),
                 trainable=True)
             FC2_b = tf.Variable(
-                initial_value=b_init(shape=(self.n_feat,), dtype="float32"), trainable=True)'''
+                initial_value=b_init(shape=(self.n_feat,), dtype="float32"), trainable=True)
             
             var_dict = {}
             var_dict['mu_rho'] = mu_rho
@@ -236,12 +236,12 @@ class ConvLayer(layers.Layer):
             var_dict['sigma_theta'] = sigma_theta
             var_dict['b_conv'] = b_conv
             var_dict['W_conv'] = W_conv
-            '''
+            
             var_dict['FC1_W'] = FC1_W
             var_dict['FC1_b'] = FC1_b
             
             var_dict['FC2_W'] = FC2_W
-            var_dict['FC2_b'] = FC2_b'''
+            var_dict['FC2_b'] = FC2_b
             
             self.variable_dicts.append(var_dict)
         
@@ -288,12 +288,12 @@ class ConvLayer(layers.Layer):
         sigma_theta = var_dict['sigma_theta']
         b_conv = var_dict['b_conv']
         W_conv = var_dict['W_conv']
-        '''
+        
         FC1_W = var_dict['FC1_W']
         FC1_b = var_dict['FC1_b']
         
         FC2_W = var_dict['FC2_W']
-        FC2_b = var_dict['FC2_b']'''
+        FC2_b = var_dict['FC2_b']
 
         
         global_desc_1 = []
@@ -330,12 +330,12 @@ class ConvLayer(layers.Layer):
             self.global_desc_1, self.n_feat, activation_fn=tf.nn.relu
         )'''
         
-        '''
+        
         ret = tf.matmul(ret, FC1_W) + FC1_b
         ret = self.relu(ret)
         
         ret = tf.matmul(ret, FC2_W) + FC2_b
-        ret = self.relu(ret)'''
+        ret = self.relu(ret)
         
         return ret
     
