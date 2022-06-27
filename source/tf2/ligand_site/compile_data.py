7ccefe319ef0645a1bf4e79c9769d33043e22fe3
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-27 00:11:55 -0400

    Update compile_data.py

--- source/tf2/ligand_site/compile_data.py
+++ source/tf2/ligand_site/compile_data.py
@@ -19,30 +19,36 @@ genPathOut = os.path.join(outDir, '{}_{}.npy')
 
 dev = '/GPU:1'
 #with tf.device(dev):
-for dataset in ['train', 'val', 'test']:
+#for dataset in ['train', 'val', 'test']:
+for dataset in ['val', 'test']:
   temp_X_files = all_files[np.char.startswith(all_files, dataset + '_X')]
-
+  
   print(dataset)
   X_list = []
   y_list = []
-  len_list = []
+  lens_list_X = []
+  lens_list_y = []
   for j, X_file in enumerate(temp_X_files):
     print(j)
     y_file = X_file.replace('X', 'y')
     X = np.load(os.path.join(datadir, X_file))
     y = np.load(os.path.join(datadir, y_file))
-    tempLen = tf.shape(X)[1]
     X_list.append(X)
     y_list.append(y)
-    len_list.append(tempLen)
-
+    lens_list_X.append(tf.shape(X)[1])
+    lens_list_y.append(tf.shape(y)[1])
+  
   #### padding X_list elements so that they all have the same dimension-2 length
-  maxLen = max(len_list)
+  maxLen_X = max(lens_list_X)
+  maxLen_y = max(lens_list_y)
   for i, X in enumerate(X_list):
-    paddings = tf.constant([[0, 0], [0, (maxLen - len_list[i]).numpy()]])
-    X_list[i] = tf.pad(X, paddings, constant_values=defaultCode)
-  ####
-
+    y = y_list[i]
+    paddings_X = tf.constant([[0, 0], [0, (maxLen_X - lens_list_X[i]).numpy()]])
+    paddings_y = tf.constant([[0, 0], [0, (maxLen_y - lens_list_y[i]).numpy()]])
+    X_list[i] = tf.pad(X, paddings_X, constant_values=defaultCode)
+    y_list[i] = tf.pad(y, paddings_y, constant_values=defaultCode)
+    ####
+  
   X = tf.concat(X_list, axis=0)
   y = tf.concat(y_list, axis=0)
   np.save(genPathOut.format(dataset, 'X'), X)
