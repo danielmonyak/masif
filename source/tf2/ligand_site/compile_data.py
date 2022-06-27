da5bbbf0163f0c67e7a9c0a43d5bcaea25140051
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-26 23:47:32 -0400

    Update compile_data.py

--- source/tf2/ligand_site/compile_data.py
+++ source/tf2/ligand_site/compile_data.py
@@ -19,34 +19,34 @@ genPathOut = os.path.join(outDir, '{}_{}.npy')
 
 dev = '/GPU:1'
 
-with tf.device(dev):
-  for dataset in ['train', 'val', 'test']:
-    temp_X_files = all_files[np.char.startswith(all_files, dataset + '_X')]
+#with tf.device(dev):
+for dataset in ['train', 'val', 'test']:
+  temp_X_files = all_files[np.char.startswith(all_files, dataset + '_X')]
 
-    print(dataset)
-    X_list = []
-    y_list = []
-    len_list = []
-    for j, X_file in enumerate(temp_X_files):
-      print(j)
-      y_file = X_file.replace('X', 'y')
-      X = np.load(os.path.join(datadir, X_file))
-      y = np.load(os.path.join(datadir, y_file))
-      tempLen = tf.shape(X)[1]
-      X_list.append(X)
-      y_list.append(y)
-      len_list.append(tempLen)
+  print(dataset)
+  X_list = []
+  y_list = []
+  len_list = []
+  for j, X_file in enumerate(temp_X_files):
+    print(j)
+    y_file = X_file.replace('X', 'y')
+    X = np.load(os.path.join(datadir, X_file))
+    y = np.load(os.path.join(datadir, y_file))
+    tempLen = tf.shape(X)[1]
+    X_list.append(X)
+    y_list.append(y)
+    len_list.append(tempLen)
 
-    #### padding X_list elements so that they all have the same dimension-2 length
-    maxLen = max(len_list)
-    for i, X in enumerate(X_list):
-      paddings = tf.constant([[0, 0], [0, (maxLen - len_list[i]).numpy()]])
-      X_list[i] = tf.pad(X, paddings, constant_values=defaultCode)
-    ####
-    
-    X = tf.concat(X_list, axis=0)
-    y = tf.concat(y_list, axis=0)
-    np.save(genPathOut.format(dataset, 'X'), X)
-    np.save(genPathOut.format(dataset, 'y'), y)
+  #### padding X_list elements so that they all have the same dimension-2 length
+  maxLen = max(len_list)
+  for i, X in enumerate(X_list):
+    paddings = tf.constant([[0, 0], [0, (maxLen - len_list[i]).numpy()]])
+    X_list[i] = tf.pad(X, paddings, constant_values=defaultCode)
+  ####
+
+  X = tf.concat(X_list, axis=0)
+  y = tf.concat(y_list, axis=0)
+  np.save(genPathOut.format(dataset, 'X'), X)
+  np.save(genPathOut.format(dataset, 'y'), y)
 
 print('Finished!')
