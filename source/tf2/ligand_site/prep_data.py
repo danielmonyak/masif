d0b45c9a8c12e5aae27dd0dccd392a9f81a486be
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-26 23:02:36 -0400

    Update prep_data.py

--- source/tf2/ligand_site/prep_data.py
+++ source/tf2/ligand_site/prep_data.py
@@ -56,7 +56,8 @@ for gpu in gpus:
 dev = '/GPU:1'
 with tf.device(dev):
     #for dataset in dataset_list.keys():
-    for dataset in ['val', 'test']:
+    for dataset in ['train']:
+    #for dataset in ['val', 'test']:
         i = 0
         j = next_epoch
 
