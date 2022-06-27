399d9b5744c8cb7277cf473e256d92902b837745
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-26 23:37:55 -0400

    Update compile_data.py

--- source/tf2/ligand_site/compile_data.py
+++ source/tf2/ligand_site/compile_data.py
@@ -9,6 +9,7 @@ from default_config.util import *
 from tf2.read_ligand_tfrecords import _parse_function
 
 params = masif_opts["ligand"]
+defaultCode = params['defaultCode']
 
 datadir = '/data02/daniel/masif/datasets/tf2/ligand_site/split'
 all_files = np.array(os.listdir(datadir))
@@ -25,12 +26,25 @@ with tf.device(dev):
     print(dataset)
     X_list = []
     y_list = []
+    len_list = []
     for j, X_file in enumerate(temp_X_files):
       print(j)
       y_file = X_file.replace('X', 'y')
-
-      X_list.append(np.load(os.path.join(datadir, X_file)))
-      y_list.append(np.load(os.path.join(datadir, y_file)))
+      X = np.load(os.path.join(datadir, X_file))
+      y = np.load(os.path.join(datadir, y_file))
+      tempLen = tf.shape(X)[0]
+      
+      X_list.append(X)
+      y_list.append(y)
+      len_list.append(tempLen)
+    
+    #### padding X_list elements so that they all have the same dimension-2 length
+    maxLen = max(len_list)
+    for i, X in enumerate(X_list):
+      paddings = tf.constant([[0, 0], [0, (maxLen - len_list[i]).numpy()]])
+      X_list[i] = tf.pad(X, paddings, constant_values=defaultCode)
+    ####
+    
     X = tf.concat(X_list, axis=0)
     y = tf.concat(y_list, axis=0)
     np.save(genPathOut.format(dataset, 'X'), X)
