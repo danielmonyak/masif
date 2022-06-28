1308341adc253f53155e9099d6048603b0558441
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-28 16:59:08 -0400

    Update fixLayer.py

--- source/tf2/ligand_site/fixLayer.py
+++ source/tf2/ligand_site/fixLayer.py
@@ -63,9 +63,10 @@ class ConvLayer(layers.Layer):
         mu_rho = []
         b_conv = []
         
-        def testFunc(**kwargs):
-            return mu_rho_initial
-        self.testVar = self.add_weight(name="mu_rho", shape=tf.shape(mu_rho_initial), initializer = ValueInit(mu_rho_initial), trainable = True)
+        def initValue(value, **kwargs):
+            return value
+        #self.testVar = self.add_weight(name="mu_rho", shape=tf.shape(mu_rho_initial), initializer = ValueInit(mu_rho_initial), trainable = True)
+        self.testVar = self.add_weight(name="mu_rho", shape=tf.shape(mu_rho_initial), initializer = initValue(mu_rho_initial), trainable = True)
         
         layer_num = 0
         for i in range(self.n_feat):
