aa07aa5e284069659c3bbe7a07a1c262da582a97
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-30 16:24:13 -0400

    Update train_individually.py

--- source/tf2/ligand_site_one/train_individually.py
+++ source/tf2/ligand_site_one/train_individually.py
@@ -8,12 +8,10 @@ from IPython.core.debugger import set_trace
 import pickle
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras import layers, Sequential, Model
 from default_config.util import *
 from tf2.read_ligand_tfrecords import _parse_function
-from tf2.ligand_site.MaSIF_ligand_site import MaSIF_ligand_site
 
-from operator import add
-from time import process_time
 
 gpus = tf.config.list_physical_devices('GPU')
 for gpu in gpus:
@@ -27,23 +25,29 @@ continue_training = False
 #############################################
 
 params = masif_opts["ligand"]
-minPockets = params['minPockets']
 
 dataset_list = {'train' : "training_data_sequenceSplit_30.tfrecord", 'val' : "validation_data_sequenceSplit_30.tfrecord", 'test' : "testing_data_sequenceSplit_30.tfrecord"}
 getData = lambda dataset : tf.data.TFRecordDataset(os.path.join(params["tfrecords_dir"], dataset_list[dataset])).map(_parse_function)
 train_data = getData('train')
 val_data = getData('val')
 
-model = MaSIF_ligand_site(
-    params["max_distance"],
-    params["n_classes"],
-    feat_mask=params["feat_mask"],
-    n_conv_layers = 1
+
+model = Sequential([
+  layers.Dense(1, activation="relu"),
+  layers.Flatten(),
+  layers.Dense(64, activation="relu"),
+  layers.Dense(20, activation='relu'),
+  layers.Dense(1)
+])
+
+opt = tf.keras.optimizers.Adam()
+loss_fn = tf.keras.losses.BinaryCrossentropy(from_logits = True)
+
+model.compile(optimizer = opt,
+  loss = loss_fn,
+  metrics=['binary_accuracy']
 )
-model.compile(optimizer = model.opt,
-    loss = model.loss_fn,
-    metrics=['binary_accuracy']
-)
+
 modelDir = 'kerasModel'
 ckpPath = os.path.join(modelDir, 'ckp')
 ckpStatePath = ckpPath + '.pickle'
@@ -65,131 +69,108 @@ else:
 def goodLabel(labels):
     n_ligands = labels.shape[1]
     if n_ligands > 1:
-        #print('More than one ligand, check this out...')
         return False
     
     pocket_points = tf.where(labels != 0)
     npoints = tf.shape(pocket_points)[0]
     if npoints < minPockets:
-        #print(f'Only {npoints} pocket_points')
         return False
     
     return True
 
+batch_threshold = 1e6
+
 with tf.device(dev):
     for i in range(last_epoch + 1, num_epochs):
+        if i == 1:
+            break
+            
         print(f'Running training data, epoch {i}')
         
-        #batch_size = 0
+        batch_size = 0
+        X_list = []
+        y_list = []
         for j, data_element in enumerate(train_data):
-            if j % 10 == 0:
-                print(f'Train record {j}')
-                verbosity = 2
-
+            if j == 10:
+                break
+            
+            if j % 1 == 0:
+                print(f'Validation record {j}')
+                
             labels = data_element[4]
             if not goodLabel(labels):
                 continue
-            
-            y_raw = tf.transpose(tf.cast(labels > 0, dtype=tf.int32))
-            
-            #flat_list = list(map(flatten, data_element[:4]))
-            #X = tf.expand_dims(tf.concat(flat_list, axis=0), axis=0)
-            #_=model.fit(X, y_raw, epochs = 1, verbose = 0)
-            
-            #####
-            n_pockets = y_raw.shape[1]
-            fullSamples = n_pockets // minPockets
-            for k in range(fullSamples):
-                '''if k % 10 == 0:
-                    done = 100.0 * k/fullSamples
-                    print('{} of {} batches completed'.format(k, fullSamples))'''
-                sample = range(minPockets * k, minPockets * (k+1))
-                
-                y_temp = tf.gather(y_raw, sample, axis=1)
-                flat_list = list(map(lambda tsr : np.take(tsr, sample, axis=0).flatten(), data_element[:4]))
-                X = tf.expand_dims(tf.concat(flat_list, axis=0), axis=0)
-                
-                _=model.fit(X, y_temp, epochs = 1, verbose = 0)
-            
-            k = fullSamples
-            n_leftover = n_pockets % minPockets
-            garbage = tf.range(minPockets * (k-1) + n_leftover, minPockets * k)
-            valid = tf.range(minPockets * k, minPockets * k + n_leftover)
-            sample = tf.concat([garbage, valid], axis=0)
-            
-            y_temp = tf.gather(y_raw, sample, axis=1)
-            flat_list = list(map(lambda tsr : np.take(tsr, sample, axis=0).flatten(), data_element[:4]))
-            X = tf.expand_dims(tf.concat(flat_list, axis=0), axis=0)
 
-            _=model.fit(X, y_temp, epochs = 1, verbose = verbosity)
-            verbosity = 0
-            #####
-            
-            
-            #batch_size += 1
-            '''
-            y, sample = model.make_y(y_raw)
-            with tf.GradientTape() as tape:
-                y_pred = model(X, sample = sample, training=True)
-                loss = model.compiled_loss(y, y_pred, regularization_losses=model.losses)
-            gradients = tape.gradient(loss, model.trainable_variables)
-            model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-            '''
-            '''if batch_size == 1:
-                tempGradients = gradients.copy()
-            else:
-                tempGradients = list(map(add, tempGradients, gradients))'''
+            y_temp = tf.cast(labels > 0, dtype=tf.int32)
+            X_temp = tf.constant(data_element[0])
+
+            X_list.append(X_temp)
+            y_list.append(y_temp)
+
+            batch_size += len(X)
+            if batch_size > batch_threshold:
+                print('a')
+                X = tf.concat(X_list, axis=0)
+                y = tf.concat(y_list, axis=0)
+                _=model.fit(X, y, epochs = 1, verbose = 2)
+                batch_size = 0
+                X_list = []
+                y_list = []
+
+        if len(X_list) > 0:
+            print('b')
+            X = tf.concat(X_list, axis=0)
+            y = tf.concat(y_list, axis=0)
+            _=model.fit(X, y, epochs = 1, verbose = 2)
         
-        #gradients = [grad / batch_size for grad in tempGradients]
-        #model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        ##############################################################
+        VALIDATION DATA
+        ##############################################################
         
         print(f'Running validation data, epoch {i}')
         acc_list = []
         loss_list = []
+        
+        batch_size = 0
+        X_list = []
+        y_list = []
         for j, data_element in enumerate(val_data):
-            if j % 10 == 0:
+            if j == 10:
+                break
+                
+            if j % 1 == 0:
                 print(f'Validation record {j}')
 
             labels = data_element[4]
             if not goodLabel(labels):
                 continue
 
-            y_raw = tf.transpose(tf.cast(labels > 0, dtype=tf.int32))
+            y_temp = tf.cast(labels > 0, dtype=tf.int32)
+            X_temp = tf.constant(data_element[0])
 
-            #flat_list = list(map(flatten, data_element[:4]))
-            #X = tf.expand_dims(tf.concat(flat_list, axis=0), axis=0)
-            #loss, acc = model.evaluate(X, y_raw, verbose = 0)            
-            #loss_list.append(loss)
-            #acc_list.append(acc)
-        
-            #####
-            n_pockets = y_raw.shape[1]
-            fullSamples = n_pockets // minPockets
-            for k in range(fullSamples):
-                sample = range(minPockets * k, minPockets * (k+1))
-                
-                y_temp = tf.gather(y_raw, sample, axis=1)
-                flat_list = list(map(lambda tsr : np.take(tsr, sample, axis=0).flatten(), data_element[:4]))
-                X = tf.expand_dims(tf.concat(flat_list, axis=0), axis=0)
-                
-                loss, acc = model.evaluate(X, y_temp, verbose = 0)            
+            X_list.append(X_temp)
+            y_list.append(y_temp)
+            
+            batch_size += len(X)
+            if batch_size > batch_threshold:
+                X = tf.concat(X_list, axis=0)
+                y = tf.concat(y_list, axis=0)
+                loss, acc = model.evaluate(X, y, verbose = 0)            
                 loss_list.append(loss)
                 acc_list.append(acc)
+                
+                batch_size = 0
+                X_list = []
+                y_list = []
             
-            k = fullSamples
-            n_leftover = n_pockets % minPockets
-            garbage = tf.range(minPockets * (k-1) + n_leftover, minPockets * k)
-            valid = tf.range(minPockets * k, minPockets * k + n_leftover)
-            sample = tf.concat([garbage, valid], axis=0)
-            
-            y_temp = tf.gather(y_raw, sample, axis=1)
-            flat_list = list(map(lambda tsr : np.take(tsr, sample, axis=0).flatten(), data_element[:4]))
-            X = tf.expand_dims(tf.concat(flat_list, axis=0), axis=0)
-
-            loss, acc = model.evaluate(X, y_temp, verbose = 0)            
+        if len(X_list) > 0:
+            print('b')
+            X = tf.concat(X_list, axis=0)
+            y = tf.concat(y_list, axis=0)
+            loss, acc = model.evaluate(X, y, verbose = 0)            
             loss_list.append(loss)
             acc_list.append(acc)
-            #####
+            
         
         acc = sum(acc_list)/len(acc_list)
         loss = sum(loss_list)/len(loss_list)
