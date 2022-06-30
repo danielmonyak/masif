86bd3d41a087998381ea9c8eabc2a4311e275b86
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-06-30 11:07:41 -0400

    Update working.py

--- source/tf2/usage/working.py
+++ source/tf2/usage/working.py
@@ -150,6 +150,11 @@ ligand_site_probs = pred.getLigandSiteProbs()
 
 def summary(threshold):
   pocket_points_pred = tf.squeeze(tf.where(ligand_site_probs > threshold))
+  
+  if len(pocket_points_pred) < minPockets:
+    print('Less than {minPockets] pocket points were predicted...')
+    return 0
+  
   overlap = np.intersect1d(pocket_points_true, pocket_points_pred)
   recall = len(overlap)/len(pocket_points_true)
   precision = len(overlap)/len(pocket_points_pred)
