a0204d2c4bafcef30e0df392b2110c37ef46f541
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-07-01 13:12:40 -0400

    Update working.py

--- source/tf2/ligand_site_one/working.py
+++ source/tf2/ligand_site_one/working.py
@@ -16,6 +16,11 @@ params = masif_opts["ligand"]
 ligand_coord_dir = params["ligand_coords_dir"]
 ligand_list = params['ligand_list']
 
+gpus = tf.config.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+strategy = tf.distribute.MirroredStrategy([gpu.name for gpu in tf.config.list_logical_devices('GPU')])
 dev = '/GPU:2'
 
 '''
