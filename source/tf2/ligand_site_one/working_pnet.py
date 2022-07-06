fceac61657e5c29ed6de1316c5bbf107c549c943
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-07-06 11:09:59 -0400

    Update working_pnet.py

--- source/tf2/ligand_site_one/working_pnet.py
+++ source/tf2/ligand_site_one/working_pnet.py
@@ -11,6 +11,7 @@ import tensorflow as tf
 from default_config.util import *
 from tf2.usage.predictor import Predictor
 
+import math
 
 params = masif_opts["ligand"]
 ligand_coord_dir = params["ligand_coords_dir"]
@@ -56,8 +57,26 @@ pred.loadData(pdb_dir)
 
 ########################
 pnet_coords = np.loadtxt(f'/home/daniel.monyak/software/PUResNet/output_folders/{pdb.split("_")[0]}/pocket0.txt', dtype=float)
-pocket_points_pred = tree.query_ball_point(pnet_coords, 3.0)
-pocket_points_pred = list(set([pp for p in pocket_points_pred for pp in p]))
+#pocket_points_pred = tree.query_ball_point(pnet_coords, 3.0)
+#pocket_points_pred = list(set([pp for p in pocket_points_pred for pp in p]))
+
+cos_part = np.identity(2)
+sin_part = np.array([[0, -1], [1, 0]])
+for theta in np.linspace(0, 2*math.pi, 10):
+    print(f'theta: {theta}')
+    rot_mat = math.cos(theta) * cos_part + math.sin(theta) * sin_part
+    coords_new = np.concatenate([np.matmul(rot_mat, pnet_coords[:, :2].T).T, np.expand_dims(pnet_coords[:, 2], axis=-1)], axis=1)
+    
+    pocket_points_pred = tree.query_ball_point(coords_new, 3.0)
+    pocket_points_pred = list(set([pp for p in pocket_points_pred for pp in p]))
+    
+    overlap = np.intersect1d(pocket_points_true, pocket_points_pred)
+    recall = len(overlap)/npoints_true
+    precision = len(overlap)/npoints
+    print('Recall:', round(recall, 2))
+    print('Precision:', round(precision, 2))
+
+
 ########################
 
 npoints = len(pocket_points_pred)
