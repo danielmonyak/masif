1b7242b690356041d686693be721b09ac8abf507
danielmonyak <daniel.monyak@arvinas.com>
2022-07-06 20:54:36 +0000

    update

--- data/masif_ligand/data_prepare_one.sh
+++ data/masif_ligand/data_prepare_one.sh
@@ -21,7 +21,7 @@ PDB_ID=$(echo $1| cut -d"_" -f1)
 CHAIN1=$(echo $1| cut -d"_" -f2)
 CHAIN2=$(echo $1| cut -d"_" -f3)
 
-python $masif_source/data_preparation/00-pdb_download.py $1
+#python $masif_source/data_preparation/00-pdb_download.py $1
 
 #conda activate venv_sbi
 
