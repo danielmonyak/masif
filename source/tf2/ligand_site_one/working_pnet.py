5bdaedfeaf37a8e447b526f57092ef51f6cfb225
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-07-06 10:25:30 -0400

    Create working_pnet.py

---
+++ source/tf2/ligand_site_one/working_pnet.py
@@ -0,0 +1,73 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
+
+import sys
+from IPython.core.debugger import set_trace
+import importlib
+import numpy as np
+from scipy import spatial
+from sklearn.metrics import accuracy_score, balanced_accuracy_score
+import tensorflow as tf
+from default_config.util import *
+from tf2.usage.predictor import Predictor
+
+
+params = masif_opts["ligand"]
+ligand_coord_dir = params["ligand_coords_dir"]
+ligand_list = params['ligand_list']
+
+
+pdb = sys.argv[1]
+
+print('pdb:', pdb)
+
+
+precom_dir = '/data02/daniel/masif/data_preparation/04a-precomputation_12A/precomputation'
+pdb_dir = os.path.join(precom_dir, pdb)
+
+xyz_coords = Predictor.getXYZCoords(pdb_dir)            
+all_ligand_coords = np.load(
+    os.path.join(
+        ligand_coord_dir, "{}_ligand_coords.npy".format(pdb.split("_")[0])
+    )
+)
+ligand_coords = all_ligand_coords[0]
+tree = spatial.KDTree(xyz_coords)
+pocket_points_true = tree.query_ball_point(ligand_coords, 3.0)
+pocket_points_true = list(set([pp for p in pocket_points_true for pp in p]))
+
+npoints_true = len(pocket_points_true)
+print(f'{npoints_true} true pocket points')
+
+all_ligand_types = np.load(
+    os.path.join(
+        ligand_coord_dir, "{}_ligand_types.npy".format(pdb.split("_")[0])
+    )
+).astype(str)
+ligand_true = all_ligand_types[0]
+ligandIdx_true = ligand_list.index(ligand_true)
+
+
+ligand_model_path = '/home/daniel.monyak/software/masif/source/tf2/usage/masif_ligand_model/savedModel'
+ligand_site_model_path = '/home/daniel.monyak/software/masif/source/tf2/ligand_site_one/kerasModel/savedModel'
+
+pred = Predictor(ligand_model_path = ligand_model_path, ligand_site_model_path = ligand_site_model_path)
+pred.loadData(pdb_dir)
+
+########################
+pnet_coords = np.loadtxt(f'/home/daniel.monyak/software/PUResNet/output_folders/{pdb.split('_')[0]}/pocket0.txt', dtype=float)
+pocket_points_pred = tree.query_ball_point(pnet_coords, 3.0)
+pocket_points_pred = list(set([pp for p in pocket_points_pred for pp in p]))
+########################
+
+
+
+X_true = pred.getLigandX(pocket_points_true)
+X_true_pred = pred.predictLigandIdx(X_true)
+print('X_true_pred:', X_true_pred.numpy())
+
+X_pred = pred.getLigandX(pocket_points_pred)
+X_pred_pred = pred.predictLigandIdx(X_pred, 0.5)
+print('\nX_pred_pred:', X_pred_pred.numpy())
+
+print('\nligandIdx_true:', ligandIdx_true)
