86c8b6aa6bcf15d9599483294f898ec3cbd6e03b
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-07-07 23:40:19 -0400

    Update evaluate.py

--- source/tf2/usage_new/evaluate.py
+++ source/tf2/usage_new/evaluate.py
@@ -1,20 +1,16 @@
 import os
 import sys
 import numpy as np
-import pandas
-from sklearn.metrics import balanced_accuracy_score
+import pandas as pd
 from scipy import spatial
 from default_config.util import *
 from tf2.usage.predictor import Predictor
-
-continue_running = False
-outdir = 'results'
 
 params = masif_opts['ligand']
 ligand_list = params['ligand_list']
 
 ligand_coord_dir = params["ligand_coords_dir"]
-precom_dir = '/data02/daniel/masif/data_preparation/04a-precomputation_12A/precomputation'
+precom_dir = params['masif_precomputation_dir']
 binding_dir = '/data02/daniel/PUresNet/site_predictions'
 
 pred = Predictor(ligand_model_path = '/home/daniel.monyak/software/masif/source/tf2/usage/masif_ligand_model/savedModel')
@@ -37,14 +33,16 @@ ligandIdx_true_list = []
 true_pts_ligandIdx_pred_list = []
 pred_pts_ligandIdx_pred_list = []
 npoints_true_list = []
-npoints_true_pred = []
+npoints_pred_list = []
 
-dataset_list = {'train' : train_list, 'test' : test_list, 'val' : val_list}
+dataset_dict = {'train' : train_list, 'test' : test_list, 'val' : val_list}
 
-for dataset in dataset_list.keys():
+for dataset in dataset_dict.keys():
+    data = dataset_list[dataset]
     n_data = len(data)
-    for i, pdb in enumerate(dataset_list[dataset]):
+    for i, pdb in enumerate(data):
         print(f'{i} of {n_data} {dataset} pdbs running...')
+        print(pdb)
         try:
             pdb_dir = os.path.join(precom_dir, pdb)
             xyz_coords = Predictor.getXYZCoords(pdb_dir)
@@ -61,7 +59,7 @@ for dataset in dataset_list.keys():
 
             npoints_true = len(pocket_points_true)
 
-            if npoints_true > 0:
+            if npoints_true == 0:
                 print('Zero true pocket points...')
                 continue
 
@@ -74,13 +72,18 @@ for dataset in dataset_list.keys():
             ligandIdx_true = ligand_list.index(ligand_true)
         except:
             continue
-
+        
+        ####################
         pred.loadData(pdb_dir)
-        true_pts_ligandIdx_pred = pred.predictLigandIdx(pred.getLigandX(pocket_points_true))
+        true_pts_ligandIdx_pred = pred.predictLigandIdx(pred.getLigandX(pocket_points_true)).numpy()
 
         ####################
         pdb_pnet_dir = os.path.join(binding_dir, pdb.rstrip("_"))
         files = os.listdir(pdb_pnet_dir)
+        if len(files) == 0:
+            print('Zero pockets were predicted...')
+            continue
+        
         n_pockets = np.sum(np.char.endswith(files, '.txt'))
 
         pocket_points_pred = []
@@ -92,21 +95,22 @@ for dataset in dataset_list.keys():
 
         npoints_pred = len(pocket_points_pred)
 
-        pred_pts_ligandIdx_pred = pred.predictLigandIdx(pred.getLigandX(pocket_points_pred))
+        pred_pts_ligandIdx_pred = pred.predictLigandIdx(pred.getLigandX(pocket_points_pred)).numpy()
 
         ####################
         overlap = np.intersect1d(pocket_points_true, pocket_points_pred)
         recall = len(overlap)/npoints_true
-        precision = len(overlap)/npoints_pred
-
+        try:
+            precision = len(overlap)/npoints_pred
+        except:
+            precision = np.NaN
+        
         ###############
-        print(pdb)
         print(f'Recall: {recall}')
         print(f'Precision: {precision}')
         print(f'True ligand: {ligandIdx_true}')
         print(f'Prediction from true pocket points: {true_pts_ligandIdx_pred}')
         print(f'Prediction from predicted pocket points: {pred_pts_ligandIdx_pred}')
-
 
         pdb_list.append(pdb)
         dataset_list.append(dataset)
@@ -118,6 +122,7 @@ for dataset in dataset_list.keys():
         npoints_true_list.append(npoints_true)
         npoints_pred_list.append(npoints_pred)
 
-columns = ['pdb_list', 'dataset_list', 'recall_list', 'precision_list', 'ligandIdx_true_list', 'true_pts_ligandIdx_pred_list', 'pred_pts_ligandIdx_pred_list', 'npoints_true', 'npoints_pred']
-results = pd.DataFrame(dict(zip([col.partition('_')[:-2] for col in columns], [eval(col) for col in columns])))
-df.to_csv('results.csv', index=False)
+columns = ['pdb_list', 'dataset_list', 'recall_list', 'precision_list', 'ligandIdx_true_list', 'true_pts_ligandIdx_pred_list', 'pred_pts_ligandIdx_pred_list', 'npoints_true_list', 'npoints_pred_list']
+results = pd.DataFrame(dict(zip([col.partition('_list')[0] for col in columns], [eval(col) for col in columns])))
+results.to_csv('results.csv', index=False)
+
