cc3293dfd5d632a29f5610b311dbd2bf584fe716
danielmonyak <daniel.monyak@arvinas.com>
2022-07-09 22:08:28 +0000

    update

--- source/tf2/masif_ligand/makeHistoryPlot.py
+++ source/tf2/masif_ligand/makeHistoryPlot.py
@@ -2,11 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-loss = np.loadtxt('loss.txt')
-val_loss = np.loadtxt('val_loss.txt')
+#loss = np.loadtxt('loss.txt')
+#val_loss = np.loadtxt('val_loss.txt')
 
-rg = range(len(loss))
-sns.lineplot(x=rg, y=loss)
-sns.lineplot(x=rg, y=val_loss)
+#rg = range(len(loss))
+#sns.lineplot(x=rg, y=loss)
+#sns.lineplot(x=rg, y=val_loss)
+
+acc = np.loadtxt('accuracy.txt')
+val_acc = np.loadtxt('val_accuracy.txt')
+
+rg = range(len(acc))
+sns.lineplot(x=rg, y=acc)
+sns.lineplot(x=rg, y=val_acc)
 
 plt.savefig('history.png')
