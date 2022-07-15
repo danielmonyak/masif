6ebf3b59acf0948a4d92be18fcb73ed6df4c1380
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-07-15 11:31:11 -0400

    Update test_Model.py

--- source/tf2/ligand_site_one/test_Model.py
+++ source/tf2/ligand_site_one/test_Model.py
@@ -57,18 +57,16 @@ class MaSIF_ligand_site(Model):
         ]'''
     
     def train_step(self, data):
+        print('Train step')
+        
         x, y = data
         
         with tf.GradientTape() as tape:
             y_pred = self(x, training=True)  # Forward pass
-            print('computing loss now')
             loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
 
-        print('computing gradient now')
         trainable_vars = self.trainable_variables
         gradients = tape.gradient(loss, trainable_vars)
-        
-        print('applying gradient now')
         self.optimizer.apply_gradients(zip(gradients, trainable_vars))
         self.compiled_metrics.update_state(y, y_pred)
         
