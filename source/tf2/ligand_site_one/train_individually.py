692adf35c37e3cb305e78c1a3b6d5ac3e92ce19e
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-07-15 12:44:04 -0400

    Update train_individually.py

--- source/tf2/ligand_site_one/train_individually.py
+++ source/tf2/ligand_site_one/train_individually.py
@@ -11,14 +11,15 @@ from default_config.util import *
 from tf2.read_ligand_tfrecords import _parse_function
 from tf2.ligand_site_one.MaSIF_ligand_site_one import MaSIF_ligand_site
 
+gpus_IN_USE = [0,1]
+
 phys_gpus = tf.config.list_physical_devices('GPU')
-strategy_str = []
-for phys_g in phys_gpus:
-    tf.config.experimental.set_memory_growth(phys_g, True)
+for i in gpus_IN_USE:
+    tf.config.experimental.set_memory_growth(phys_gpus[i], True)
 
 lg_gpus = tf.config.experimental.list_logical_devices('GPU')
 gpus_str = [g.name for g in lg_gpus]
-strategy = tf.distribute.MirroredStrategy(gpus_str)
+strategy = tf.distribute.MirroredStrategy([gpus_str[i] for i in gpus_IN_USE])
 
 dev = '/GPU:1'
 cpu = '/CPU:0'
