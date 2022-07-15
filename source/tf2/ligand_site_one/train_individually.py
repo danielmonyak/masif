4b44d16980e97601ac0c3e761d3b034664f15757
danielmonyak <daniel.monyak@arvinas.com>
2022-07-15 16:03:09 +0000

    udpate

--- source/tf2/ligand_site_one/train_individually.py
+++ source/tf2/ligand_site_one/train_individually.py
@@ -9,7 +9,7 @@ import numpy as np
 import tensorflow as tf
 from default_config.util import *
 from tf2.read_ligand_tfrecords import _parse_function
-from tf2.ligand_site_one.MaSIF_ligand_site_one import MaSIF_ligand_site
+from tf2.ligand_site_one.test_Model import MaSIF_ligand_site
 
 phys_gpus = tf.config.list_physical_devices('GPU')
 strategy_str = []
@@ -66,13 +66,6 @@ def goodLabel(labels):
     return True
 
 max_verts = 200
-'''
-def pad_map_fn(packed):
-    row, def_val = packed
-    leftover = max_verts - row.shape[0]
-    paddings = tf.constant([[0,leftover]])
-    return tf.pad(row, paddings, constant_values=def_val)'''
-
 def pad_indices(indices, max_verts):
     ret_list = []
     for patch_ix in range(len(indices)):
@@ -88,7 +81,8 @@ with strategy.scope():
         params["max_distance"],
         params["n_classes"],
         feat_mask=params["feat_mask"],
-        n_conv_layers = 1
+        n_conv_layers = 3,
+        conv_batch_size = None
     )
 
     from_logits = model.loss_fn.get_config()['from_logits']
@@ -133,16 +127,9 @@ with strategy.scope():
         #############################################################
         finished_samples = 0
         
-        while finished_samples < train_samples_threshold:
-            optional = train_iterator.get_next_as_optional()
-            if optional.has_value():
-                data_element = optional.get_value()
-            else:
-                train_iterator = iter(train_data)
-                train_j = 0
-                i += 1
-                print(f'Running training data, epoch {i}')
-                continue
+        optional = train_iterator.get_next_as_optional()
+        while optional.has_value():
+            data_element = optional.get_value()
                 
             print(f'Epoch {i}, train record {train_j}')
 
@@ -151,139 +138,29 @@ with strategy.scope():
                 train_j += 1
                 continue
             
-            y = tf.cast(labels > 0, dtype=tf.int32)
-            
             pdb = data_element[5].numpy().decode('ascii') + '_'
             indices = np.load(os.path.join(params['masif_precomputation_dir'], pdb, 'p1_list_indices.npy'), encoding="latin1", allow_pickle = True)
+            indices = pad_indices(indices, max_verts)
             
-            '''default_values = tf.range(indices.shape[0])
-            indices = tf.map_fn(fn=pad_map_fn, elems=(indices, default_values), fn_output_signature=tf.TensorSpec(shape=max_verts, dtype=tf.int32))'''
-            indices = np.expand_dims(pad_indices(indices, max_verts), axis=-1)
-            
-            
-            '''pocket_points = np.where(np.squeeze(labels > 0))[0]
-            npoints = pocket_points.shape[0]
-            empty_points = np.where(np.squeeze(labels == 0))[0]
-            #empty_sample = tf.random.shuffle(empty_points)[:npoints]
-            empty_sample = np.random.choice(empty_points, npoints)
-            
-            #sample = flatten(tf.concat([pocket_points, empty_sample], axis=0))
-            sample = np.concatenate([pocket_points, empty_sample])'''
-            
-            #coords = [tf.expand_dims(tsr, axis=-1) for tsr in data_element[1:3]]
-            #X = tf.expand_dims(tf.concat([data_element[0]] + coords + [data_element[3], indices], axis=-1), axis=0)
-            coords = [np.expand_dims(tsr, axis=-1) for tsr in data_element[1:3]]
-            X = np.concatenate([data_element[0]] + coords + [data_element[3], indices], axis=-1)
-            
-            '''y_samp = tf.gather(y, sample)
-            X_samp = X[sample]'''
-            
-            batch_sz = X.shape[0]
+            X = (data_element[:4], indices)
+            y = tf.cast(labels > 0, dtype=tf.int32)
+            batch_sz = y.shape[0]
             
             #class_weight = {0 : 1.0, 1 : 20.0})
-            
-            model.fit(X, y, verbose = 1, batch_size = batch_sz, use_multiprocessing = True)
-
-            '''
-            ##########################################
-            ##########################################
-            with tf.GradientTape() as tape:
-                y_pred = model(X, training=True)  # Forward pass
-                print('computing loss now')
-                loss = model.compiled_loss(y, y_pred, regularization_losses=model.losses)
-
-            print('computing gradient now')
-            trainable_vars = model.trainable_variables
-            gradients = tape.gradient(loss, trainable_vars)
-
-            print('applying gradient now')
-            model.optimizer.apply_gradients(zip(gradients, trainable_vars))
-            model.compiled_metrics.update_state(y, y_pred)
-            ##########################################
-            ##########################################
-            '''
+            model.fit(X, y, verbose = 2, use_multiprocessing = True)
             
             print('\n\nFinished training on one protein\n\n')
-            
             finished_samples += batch_sz
             train_j += 1
+        
+        train_iterator = iter(train_data)
+        train_j = 0
+        i += 1
         
         model.save_weights(ckpPath)
         ckpState = {'best_acc' : best_acc, 'last_epoch' : i}
         with open(ckpStatePath, 'wb') as handle:
             pickle.dump(ckpState, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        
-        '''
-        #############################################################
-        ###################    VALIDATION DATA    ###################
-        #############################################################
-        print(f'Testing model on validation data after training on {finished_samples} samples')
-        
-        finished_samples = 0
-        
-        acc_list = []
-        loss_list = []
 
-        while finished_samples < val_samples_threshold:
-            optional = val_iterator.get_next_as_optional()
-            if optional.has_value():
-                data_element = optional.get_value()
-            else:
-                val_iterator = iter(val_data)
-                val_j = 0
-                continue
-            
-            print(f'Validation record {val_j}')
-
-            labels = data_element[4]
-            if not goodLabel(labels):
-                continue
-
-            pdb = data_element[5].numpy().decode('ascii') + '_'
-            indices = np.load(os.path.join(params['masif_precomputation_dir'], pdb, 'p1_list_indices.npy'), encoding="latin1", allow_pickle = True)
-            indices = np.expand_dims(pad_indices(indices, max_verts), axis=-1)
-            
-            pocket_points = np.where(np.squeeze(labels > 0))[0]
-            npoints = pocket_points.shape[0]
-            empty_points = np.where(np.squeeze(labels == 0))[0]
-            empty_sample = np.random.choice(empty_points, npoints)
-            
-            sample = np.concatenate([pocket_points, empty_sample])
-            
-            coords = [np.expand_dims(tsr, axis=-1) for tsr in data_element[1:3]]
-            X = np.concatenate([data_element[0]] + coords + [data_element[3], indices], axis=-1)
-            y = tf.cast(labels > 0, dtype=tf.int32)
-            
-            y_samp = tf.gather(y, sample)
-            X_samp = X[sample]
-            
-            batch_sz = X.shape[0]
-            
-            loss, acc = model.evaluate(X, y, verbose = 0, batch_size = batch_sz)            
-            loss_list.append(loss)
-            acc_list.append(acc)
-
-            finished_samples += batch_sz
-            val_j += 1
-        
-        #############################################################
-        #############    EVALUATING VALIDATION DATA    ##############
-        #############################################################
-        
-        acc = sum(acc_list)/len(acc_list)
-        loss = sum(loss_list)/len(loss_list)
-        print(f'Finished evaluating {finished_samples} validation samples\nLoss: {round(loss, 2)}\nBinary Accuracy: {round(acc, 2)}')
-
-        if acc > best_acc:
-            print(f'Validation accuracy improved from {best_acc} to {acc}')
-            print(f'Saving model weights to {ckpPath}')
-            best_acc = acc
-            model.save_weights(ckpPath)
-            ckpState = {'best_acc' : best_acc, 'last_epoch' : i}
-            with open(ckpStatePath, 'wb') as handle:
-                pickle.dump(ckpState, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        else:
-            print(f'Validation accuracy did not improve from {best_acc}')
-'''
 model.save(modelPath_endTraining)
             
