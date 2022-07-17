b1331f6730f0f913cdb2ba95f0cce6802ec8f40f
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-07-16 21:42:37 -0400

    Update MaSIF_ligand_site_one.py

--- source/tf2/ligand_site_one/MaSIF_ligand_site_one.py
+++ source/tf2/ligand_site_one/MaSIF_ligand_site_one.py
@@ -240,9 +240,9 @@ class ConvLayer(layers.Layer):
             sampIdx = tf.stack([0, n_samples], axis=0)
         else:
             leftover = self.conv_batch_size - (n_samples % self.conv_batch_size)
-            def addLeftover (tsr, dtype):
+            def addLeftover(tsr, dtype):
                 shape = tf.concat([tf.expand_dims(leftover, axis=0), tsr.shape[1:]], axis=0)
-                empty = tf.zeros(shape)
+                empty = tf.zeros(shape, dtype=dtype)
                 return tf.concat([tsr, empty], axis=0)
             input_feat, rho_coords, theta_coords, mask = (addLeftover(tsr, tf.float32) for tsr in x[0])
             indices_tensor = addLeftover(x[1], tf.int32)
