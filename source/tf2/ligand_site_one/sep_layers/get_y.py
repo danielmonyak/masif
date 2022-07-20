dc63b26b1481d3d3ee0f2867ad5ddec390fe89fe
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-07-20 18:15:01 -0400

    Create get_y.py

---
+++ source/tf2/ligand_site_one/sep_layers/get_y.py
@@ -0,0 +1,60 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
+import sys
+import numpy as np
+from scipy import spatial
+import tensorflow as tf
+from default_config.util import *
+
+params = masif_opts["ligand_site"]
+
+def get_data(pdb_id):
+    mydir = os.path.join(params["masif_precomputation_dir"], pdb_id + '_')
+
+    mask = np.load(os.path.join(mydir, "p1_mask.npy"))
+    n_samples = mask.shape[0]
+
+    if n_samples > 8000:
+        return None
+
+    input_feat = np.load(os.path.join(mydir, "p1_input_feat.npy"))
+    rho_coords = np.load(os.path.join(mydir, "p1_rho_wrt_center.npy"))
+    theta_coords = np.load(os.path.join(mydir, "p1_theta_wrt_center.npy"))
+    mask = np.expand_dims(mask, 2)
+    indices = np.load(os.path.join(mydir, "p1_list_indices.npy"), encoding="latin1", allow_pickle = True)
+    # indices is (n_verts x <30), it should be
+    indices = pad_indices(indices, mask.shape[1]).astype(np.int32)
+
+    data_tsrs = tuple(np.expand_dims(tsr, axis=0) for tsr in [input_feat, rho_coords, theta_coords, mask])
+    indices = np.expand_dims(indices, axis=0)
+    X = (data_tsrs, indices)
+
+    ###############################################################
+    ###############################################################
+    X_coords = np.load(os.path.join(mydir, "p1_X.npy"))
+    Y_coords = np.load(os.path.join(mydir, "p1_Y.npy"))
+    Z_coords = np.load(os.path.join(mydir, "p1_Z.npy"))
+    xyz_coords = np.vstack([X_coords, Y_coords, Z_coords ]).T
+    tree = spatial.KDTree(xyz_coords)
+    coordsPath = os.path.join(
+        params['ligand_coords_dir'], "{}_ligand_coords.npy".format(pdb_id.split("_")[0])
+    )
+    try:
+        all_ligand_coords = np.load(coordsPath)
+    except:
+        return None
+
+    pocket_points = []
+    for j, structure_ligand in enumerate(all_ligand_coords):
+        ligand_coords = all_ligand_coords[j]
+        temp_pocket_points = tree.query_ball_point(ligand_coords, 3.0)
+        temp_pocket_points = list(set([pp for p in temp_pocket_points for pp in p]))
+        pocket_points.extend(temp_pocket_points)
+
+    y = np.zeros([1, n_samples, 1], dtype=np.int32)
+    y[0, pocket_points, 0] = 1
+
+    if (np.mean(y) > 0.75) or (np.sum(y) < 30):
+        return None
+    
+    return X, y
