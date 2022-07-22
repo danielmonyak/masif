f53266d24f53ac36b75a63d79c995b605bc3263f
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-07-22 09:44:29 -0400

    Update and rename working.py to predict.py

---
+++ source/tf2/LSResNet/predict.py
@@ -0,0 +1,64 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+import sys
+from IPython.core.debugger import set_trace
+import importlib
+import numpy as np
+from scipy import spatial
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, roc_auc_score, RocCurveDisplay
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+phys_gpus = tf.config.list_physical_devices('GPU')
+for phys_g in phys_gpus:
+    tf.config.experimental.set_memory_growth(phys_g, True)
+
+from default_config.util import *
+from tf2.usage.predictor import Predictor
+
+params = masif_opts["ligand_site"]
+ligand_coord_dir = params["ligand_coords_dir"]
+ligand_list = params['ligand_list']
+
+possible_test_pdbs = ['2VRB_AB_', '1FCD_AC_', '1FNN_A_', '1RI4_A_', '4PGH_AB_']
+possible_train_pdbs = ['4X7G_A_', '4RLR_A_', '3OWC_A_', '3SC6_A_', '1TU9_A_']
+pos_list = {'test' : possible_test_pdbs, 'train' : possible_train_pdbs}
+
+if len(sys.argv) > 2:
+  pdb_idx = int(sys.argv[1])
+  possible_pdbs = pos_list[sys.argv[2]]
+  pdb = possible_pdbs[pdb_idx]
+else:
+  pdb = sys.argv[1]
+
+print('pdb:', pdb)
+
+pdb_dir = os.path.join(params['masif_precomputation_dir'], pdb)
+xyz_coords = Predictor.getXYZCoords(pdb_dir)
+
+model = LSResNet(
+    params["max_distance"],
+    feat_mask=params["feat_mask"],
+    n_thetas=4,
+    n_rhos=3,
+    learning_rate = 1e-4,
+    n_rotations=4,
+    reg_val = 0
+)
+from_logits = model.loss_fn.get_config()['from_logits']
+thresh = (not from_logits) * 0.5
+binAcc = tf.keras.metrics.BinaryAccuracy(threshold = thresh)
+auc = tf.keras.metrics.AUC(from_logits = from_logits)
+model.compile(optimizer = model.opt,
+  loss = model.loss_fn,
+  metrics=[binAcc, auc]
+)
+
+
+modelDir = 'kerasModel'
+ckpPath = os.path.join(modelDir, 'ckp')
+
+load_status = model.load_weights(ckpPath)
+#load_status.expect_partial()
+
+X, y = get_data(pdb)
