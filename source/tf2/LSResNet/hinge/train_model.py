4d90ce5e1ccb94bbd9b41998e8c2bab34ebe5648
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-07-22 14:21:07 -0400

    Update train_model.py

--- source/tf2/LSResNet/hinge/train_model.py
+++ source/tf2/LSResNet/hinge/train_model.py
@@ -42,12 +42,6 @@ model = LSResNet(
     reg_val = 0
 )
 
-def hinge_accuracy(y_true, y_pred):
-    y_true = tf.squeeze(y_true) > 0.0
-    y_pred = tf.squeeze(y_pred) > 0.0
-    result = tf.cast(y_true == y_pred, tf.float32)
-    return tf.reduce_mean(result)
-
 def F1(y_true, y_pred):
     y_true = tf.squeeze(y_true) > 0.0
     y_pred = tf.squeeze(y_pred) > 0.0
