350c6f5eaa60daaab466411b61ef317a21b854a4
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-07-22 14:22:12 -0400

    Update train_model.py

--- source/tf2/LSResNet/hinge/train_model.py
+++ source/tf2/LSResNet/hinge/train_model.py
@@ -42,15 +42,11 @@ model = LSResNet(
     reg_val = 0
 )
 
-def F1(y_true, y_pred):
+def hinge_accuracy(y_true, y_pred):
     y_true = tf.squeeze(y_true) > 0.0
     y_pred = tf.squeeze(y_pred) > 0.0
-    overlap = tf.reduce_sum(tf.cast(y_true & y_pred, dtype=tf.float32))
-    n_true = tf.reduce_sum(tf.cast(y_true, dtype=tf.float32))
-    n_pred = tf.reduce_sum(tf.cast(y_pred, dtype=tf.float32))
-    recall = overlap/n_true
-    precision = overlap/n_pred
-    return 2*precision*recall / (precision + recall)
+    result = tf.cast(y_true == y_pred, tf.float32)
+    return tf.reduce_mean(result)
 
 binAcc = tf.keras.metrics.BinaryAccuracy(threshold = 0)
 model.compile(optimizer = 'adam',
