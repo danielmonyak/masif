f23df9456db64317d20645aa050747404ac63391
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-07-22 14:17:05 -0400

    Update train_model.py

--- source/tf2/LSResNet/hinge/train_model.py
+++ source/tf2/LSResNet/hinge/train_model.py
@@ -48,10 +48,20 @@ def hinge_accuracy(y_true, y_pred):
     result = tf.cast(y_true == y_pred, tf.float32)
     return tf.reduce_mean(result)
 
+def F1(y_true, y_pred):
+    y_true = tf.squeeze(y_true) > 0.0
+    y_pred = tf.squeeze(y_pred) > 0.0
+    overlap = tf.reduce_sum(tf.cast(y_true & y_pred, dtype=tf.float32))
+    n_true = tf.reduce_sum(tf.cast(y_true, dtype=tf.float32))
+    n_pred = tf.reduce_sum(tf.cast(y_pred, dtype=tf.float32))
+    recall = overlap/n_true
+    precision = overlap/n_pred
+    return 2*precision*recall / (precision + recall)
+
 binAcc = tf.keras.metrics.BinaryAccuracy(threshold = 0)
 model.compile(optimizer = 'adam',
   loss = 'hinge',
-  metrics=[hinge_accuracy]
+  metrics=[hinge_accuracy, F1]
 )
 
 if continue_training:
@@ -101,19 +111,22 @@ for i in range(num_epochs):
     
     loss_list = []
     acc_list = []
+    F1_list = []
     for pdb_id in val_list:
         data = get_data(pdb_id)
         if data is None:
             continue
             
         X, y = data
-        loss, acc = model.evaluate(X, y, verbose=0)[:3]
+        loss, acc, F1 = model.evaluate(X, y, verbose=0)[:3]
         loss_list.append(loss)
         acc_list.append(acc)
+        F1_list.append(F1)
     
     print(f'Epoch {i}, Validation Metrics')
     print(f'Loss: {np.mean(loss_list)}')
-    print(f'Binary Accuracy: {np.mean(acc_list)}')
+    print(f'Accuracy: {np.mean(acc_list)}')
+    print(f'F1: {np.mean(F1_list)}')
     
     print(f'Saving model weights to {ckpPath}')
     model.save_weights(ckpPath)
