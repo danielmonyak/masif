696dced7b15b892f7ed03befe04e44952358c02e
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-07-25 09:41:55 -0400

    Update train_model.py

--- source/tf2/ligand_site_one/train_model.py
+++ source/tf2/ligand_site_one/train_model.py
@@ -1,9 +1,6 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
 import sys
-import importlib
-from IPython.core.debugger import set_trace
-import pickle
 import numpy as np
 from scipy import spatial
 import tensorflow as tf
@@ -14,21 +11,16 @@ for phys_g in phys_gpus:
     tf.config.experimental.set_memory_growth(phys_g, True)
 
 from default_config.util import *
-from tf2.ligand_site_one.sep_layers.MaSIF_ligand_site_one import MaSIF_ligand_site
+from MaSIF_ligand_site_one import MaSIF_ligand_site
 from get_data import get_data
 #############################################
 continue_training = False
-#read_metrics = False
 #############################################
 
-#params = masif_opts["ligand"]
 params = masif_opts["ligand_site"]
 
 modelDir = 'kerasModel'
 ckpPath = os.path.join(modelDir, 'ckp')
-ckpStatePath = ckpPath + '.pickle'
-
-modelPath_endTraining = os.path.join(modelDir, 'savedModel_endTraining')
 
 #############################################
 #############################################
@@ -52,29 +44,15 @@ from_logits = model.loss_fn.get_config()['from_logits']
 thresh = (not from_logits) * 0.5
 binAcc = tf.keras.metrics.BinaryAccuracy(threshold = thresh)
 auc = tf.keras.metrics.AUC(from_logits = from_logits)
-TP = myMetrics.TruePositives(from_logits = from_logits)
-TN = myMetrics.TrueNegatives(from_logits = from_logits)
-FP = myMetrics.FalsePositives(from_logits = from_logits)
-FN = myMetrics.FalseNegatives(from_logits = from_logits)
 
 model.compile(optimizer = model.opt,
   loss = model.loss_fn,
-  metrics=[binAcc, auc, TP, FN]
+  metrics=[binAcc, auc, F1]
 )
 
 if continue_training:
     model.load_weights(ckpPath)
     print(f'Loaded model from {ckpPath}')
-
-'''
-if read_metrics:
-    with open(ckpStatePath, 'rb') as handle:
-        ckpState = pickle.load(handle)
-    starting_epoch = ckpState['last_epoch']
-    print(f'Resuming epoch {i} of training\nValidation accuracy: {best_acc}')
-else:
-    i = 0
-    best_acc = 0'''
 
 training_list = np.load('/home/daniel.monyak/software/masif/data/masif_ligand/lists/train_pdbs_sequence.npy')
 val_list = np.load('/home/daniel.monyak/software/masif/data/masif_ligand/lists/val_pdbs_sequence.npy')
@@ -92,7 +70,7 @@ for i in range(num_epochs):
     ###################     TRAINING DATA     ###################
     #############################################################
     for pdb_id in training_list:
-        data = get_data(pdb_id)
+        data = get_data(pdb_id, make_sample_weight = False)
         if data is None:
             continue
         
@@ -113,6 +91,7 @@ for i in range(num_epochs):
     loss_list = []
     acc_list = []
     auc_list = []
+    F1_list = []
     for pdb_id in val_list:
         data = get_data(pdb_id)
         if data is None:
@@ -125,15 +104,17 @@ for i in range(num_epochs):
             sample_weight = None
             X, y = data
         
-        loss, acc, auc = model.evaluate(X, y, verbose=0)[:3]
+        loss, acc, auc, F1_  = model.evaluate(X, y, verbose=0)[:4]
         loss_list.append(loss)
         acc_list.append(acc)
         auc_list.append(auc)
+        F1_list.append(F1_)
     
     print(f'Epoch {i}, Validation Metrics')
     print(f'Loss: {np.mean(loss_list)}')
     print(f'Binary Accuracy: {np.mean(acc_list)}')
     print(f'AUC: {np.mean(auc_list)}')
+    print(f'F1: {np.mean(F1_list)}')
     
     print(f'Saving model weights to {ckpPath}')
     model.save_weights(ckpPath)
