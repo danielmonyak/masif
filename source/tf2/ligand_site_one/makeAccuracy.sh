cff841dc8eb70930e9efdc82d9537810d78782d1
danielmonyak <daniel.monyak@arvinas.com>
2022-07-25 13:32:34 +0000

    update

--- source/tf2/ligand_site_one/makeAccuracy.sh
+++ source/tf2/ligand_site_one/makeAccuracy.sh
@@ -1,2 +1,2 @@
-pcregrep -o2 "( loss\: )([\.0-9]+)" train_individually.out > loss.txt
-pcregrep -o2 "( auc\: )([\.0-9]+)" train_individually.out > auc.txt
+pcregrep -o2 "( auc\: )([\.0-9]+)" train_model.out > auc.txt
+pcregrep -o2 "( loss\: )([\.0-9]+)" train_model.out > loss.txt
