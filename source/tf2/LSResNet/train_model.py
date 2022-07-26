b0f079ce240521fd3fbf57e44e6383da4cee7369
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-07-26 09:55:07 -0400

    Update train_model.py

--- source/tf2/LSResNet/train_model.py
+++ source/tf2/LSResNet/train_model.py
@@ -19,8 +19,6 @@ from get_data import get_data
 
 params = masif_opts["LSResNet"]
 
-
-
 #############################################
 #############################################
 #num_epochs = 200                #############
@@ -30,30 +28,7 @@ train_batch_sz_threshold = 32   #############
 #############################################
 #############################################
 
-continue_key = input('Continue training from checkpoint? (y/[n]): ')
-if (continue_key == '') or (continue_key == 'n'):
-    continue_training = False
-elif continue_key == 'y':
-    continue_training = True
-else:
-    sys.exit('Please enter a valid choice...')
-
-if continue_training:
-    ckpPath = os.path.join('kerasModel', 'ckp')
-    ckpKey = input(f'Using checkpoint at {ckpPath}? ([y]/n): ')
-    if (ckpKey != '') and (ckpKey != 'y'):
-        if ckpKey == 'n':
-            ckpPath = input('Enter checkpoint path: ')
-        else:
-            sys.exit('Please enter a valid choice...')
-    
-    starting_epoch = int(input('Starting epoch: '))
-else:
-    ckpPath = os.path.join('kerasModel', 'ckp')
-    starting_epoch = 0
-
-num_epochs = int(input('Enter the number of epochs to train for: '))
-
+from input_helper import *
 
 print(f'Training for {num_epochs} epochs')
 if continue_training:
