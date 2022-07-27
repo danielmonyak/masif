d8c55c15d11dcf10126408d4e7ade1f7a3f32ac6
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-07-27 15:48:28 -0400

    Update util.py

--- source/default_config/util.py
+++ source/default_config/util.py
@@ -2,9 +2,6 @@ import numpy as np
 import tensorflow as tf
 from default_config.masif_opts import masif_opts
 
-params = masif_opts["ligand"]
-minPockets = params['minPockets']
-
 inputFeatSpec = tf.RaggedTensorSpec(shape=[None, 200, 5], dtype=tf.float32)
 restSpec = tf.RaggedTensorSpec(shape=[None, 200, 1], dtype=tf.float32)
 sampleSpec = tf.TensorSpec([minPockets], dtype=tf.int32)
