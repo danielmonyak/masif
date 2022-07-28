4d3195940bd2704bd95587044e175b663be4bc6d
danielmonyak <daniel.monyak@arvinas.com>
2022-07-28 00:45:57 +0000

    update

--- source/tf2/LSResNet/hinge/makeLossPlot.py
+++ source/tf2/LSResNet/hinge/makeLossPlot.py
@@ -2,7 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
+#loss_most = np.loadtxt('loss_most.txt')
 loss = np.loadtxt('loss.txt')
+
+#loss = np.concatenate([loss_most, loss])
+
 rg = range(0, loss.shape[0])
 sns.lineplot(x=rg, y=loss)
 
