1192de9148d8cfa383e475833028412c883dea6c
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-07-31 18:33:50 -0400

    Update get_data.py

--- source/tf2/masif_ligand/stochastic/get_data.py
+++ source/tf2/masif_ligand/stochastic/get_data.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy import spatial
 from default_config.util import *
 
-params = masif_opts["ligand_site"]
+params = masif_opts["ligand"]
 
 def get_data(pdb_id, training = True):
     mydir = os.path.join(params["masif_precomputation_dir"], pdb_id.rstrip('_') + '_')
@@ -11,21 +11,13 @@ def get_data(pdb_id, training = True):
     mask = np.load(os.path.join(mydir, "p1_mask.npy"))
     n_samples = mask.shape[0]
 
-    if training and n_samples > 8000:
-        return None
-
     input_feat = np.load(os.path.join(mydir, "p1_input_feat.npy"))
     rho_coords = np.load(os.path.join(mydir, "p1_rho_wrt_center.npy"))
     theta_coords = np.load(os.path.join(mydir, "p1_theta_wrt_center.npy"))
     mask = np.expand_dims(mask, 2)
-    indices = np.load(os.path.join(mydir, "p1_list_indices.npy"), encoding="latin1", allow_pickle = True)
-    # indices is (n_verts x <30), it should be
-    indices = pad_indices(indices, mask.shape[1]).astype(np.int32)
-
-    data_tsrs = tuple(np.expand_dims(tsr, axis=0) for tsr in [input_feat, rho_coords, theta_coords, mask])
-    indices = np.expand_dims(indices, axis=0)
-    X = (data_tsrs, indices)
-
+    
+    X = tuple(np.expand_dims(tsr, axis=0) for tsr in [input_feat, rho_coords, theta_coords, mask])
+    
     ###############################################################
     ###############################################################
     X_coords = np.load(os.path.join(mydir, "p1_X.npy"))
@@ -47,25 +39,12 @@ def get_data(pdb_id, training = True):
         ligand_coords = all_ligand_coords[j]
         temp_pocket_points = tree.query_ball_point(ligand_coords, 3.0)
         temp_pocket_points = list(set([pp for p in temp_pocket_points for pp in p]))
-        if len(temp_pocket_points) > 32:
-            pocket_points.extend(temp_pocket_points)
+        temp_npoints = len(temp_pocket_points)
+        if (temp_npoints > minPockets) and (temp_npoints/n_samples < 0.75):
+            pocket_points.append(temp_pocket_points)
 
-    if len(pocket_points) == 0:
-        print(f'{pdb_id} has no pockets big enough...')
+    if len(pocket_points) < minPockets:
+        #print(f'{pdb_id} has no pockets big enough...')
         return None
-            
-    y = np.zeros([1, n_samples, 1], dtype=np.int32)
-    y[0, pocket_points, 0] = 1
 
-    if np.mean(y) > 0.75:
-        print(f'{pdb_id} is weird...')
-        return None
-    
-    n_pockets = np.sum(y)
-    n_empty = n_samples - n_pockets
-
-    sample_weight = np.empty(shape=y.shape, dtype=np.float32)
-    sample_weight.fill(n_samples/(2*n_empty))
-    sample_weight[0, pocket_points, 0] = n_samples/(2*n_pockets)
-
-    return X, y, sample_weight
+    return X, pocket_points
