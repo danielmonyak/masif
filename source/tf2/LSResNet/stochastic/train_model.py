4e2375aa8783b6a073b8a4bc2b9e36d2879ff0c9
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-08-01 12:28:52 -0400

    Update train_model.py

--- source/tf2/LSResNet/stochastic/train_model.py
+++ source/tf2/LSResNet/stochastic/train_model.py
@@ -65,30 +65,83 @@ if continue_training:
     model.load_weights(ckpPath)
     print(f'Loaded model from {ckpPath}')
 
-
-optimizer = tf.keras.optimizers.SGD(learning_rate=lr)
-loss_fn = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-
-def F1_lower(y_true, y_pred): return util.F1(y_true, y_pred, threshold=0.4)
-def F1_upper(y_true, y_pred): return util.F1(y_true, y_pred, threshold=0.6)
-
 from_logits = True
+optimizer = tf.keras.optimizers.SGD(learning_rate=lr)
+loss_fn = tf.keras.losses.BinaryCrossentropy(from_logits=from_logits)
+
 
 train_acc_metric = tf.keras.metrics.BinaryAccuracy(threshold = (not from_logits) * 0.5)
-train_auc_metric = tf.keras.metrics.AUC(from_logits = from_logits)
-train_F1_lower_metric = util.F1_Metric(from_logits = from_logits, threshold = 0.3)
-train_F1_metric = util.F1_Metric(from_logits = from_logits, threshold = 0.5)
+train_auc_metric = tf.keras.metrics.AUC(from_logits=from_logits)
+train_F1_lower_metric = util.F1_Metric(from_logits=from_logits, threshold = 0.3)
+train_F1_metric = util.F1_Metric(from_logits=from_logits, threshold = 0.5)
 
 val_acc_metric = tf.keras.metrics.BinaryAccuracy(threshold = (not from_logits) * 0.5)
-val_auc_metric = tf.keras.metrics.AUC(from_logits = from_logits)
-val_F1_lower_metric = util.F1_Metric(from_logits = from_logits, threshold = 0.3)
-val_F1_metric = util.F1_Metric(from_logits = from_logits, threshold = 0.5)
+val_auc_metric = tf.keras.metrics.AUC(from_logits=from_logits)
+val_F1_lower_metric = util.F1_Metric(from_logits=from_logits, threshold = 0.3)
+val_F1_metric = util.F1_Metric(from_logits=from_logits, threshold = 0.5)
 
+@tf.function
+def train_step(x, y):
+    with tf.GradientTape() as tape:
+        logits = model(x, training=True)
+        loss_value = loss_fn(y, logits)
+    grads = tape.gradient(loss_value, model.trainable_weights)
+    optimizer.apply_gradients(zip(grads, model.trainable_weights))
+    train_acc_metric.update_state(y, logits)
+    train_auc_metric.update_state(y, logits)
+    train_F1_lower_metric.update_state(y, logits)
+    train_F1_metric.update_state(y, logits)
+    return loss_value
 
+@tf.function
+def test_step(x, y):
+    val_logits = model(x, training=False)
+    val_acc_metric.update_state(y, val_logits)
+    val_acc_metric.update_state(y, logits)
+    val_auc_metric.update_state(y, logits)
+    val_F1_lower_metric.update_state(y, logits)
+    val_F1_metric.update_state(y, logits)
 
 iterations = starting_iteration
-
-cur_batch_sz = 0
+while iterations < num_iterations:
+    i = 0
+    loss_list = []
+    while i < n_train:
+        try:
+            pdb_id = next(train_iter)
+        except:
+            np.random.shuffle(train_list)
+            train_iter = iter(train_list)
+            print('\nReshuffling training set...')
+            continue
+        
+        data = get_data(pdb_id)
+        if data is None:
+            continue
+            
+        X, y = data
+        loss_value = train_step(X, y)
+        loss_list.append(loss_value)
+        i += 1
+        iterations += 1
+    
+    mean_loss = np.mean(loss_list)
+    train_acc = train_acc_metric.result()
+    train_auc = train_auc_metric.result()
+    train_F1_lower = train_F1_lower_metric.result()
+    train_F1 = train_F1_metric.result()
+    
+    print(f'\nTRAINING results over {i} PDBs') 
+    print("Loss --------------------- %.4f" % (mean_loss,))
+    print("Accuracy ----------------- %.4f" % (float(train_acc),))
+    print("AUC      ----------------- %.4f" % (float(train_auc),))
+    print("F1 Lower ----------------- %.4f" % (float(train_F1_lower),))
+    print("F1       ----------------- %.4f" % (float(train_F1),))
+    
+    print(f'{iterations} iterations completed')
+    
+    train_acc_metric.reset_states()
+            
 print(f'Running training data, epoch {i}')
 for i in range(num_epochs):
     if i < starting_epoch:
