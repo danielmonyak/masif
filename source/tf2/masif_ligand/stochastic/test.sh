e2297e412f17d9e35b3378fed489396609171fcc
danielmonyak <daniel.monyak@arvinas.com>
2022-08-01 14:42:41 +0000

    u

---
+++ source/tf2/masif_ligand/stochastic/test.sh
@@ -0,0 +1,2 @@
+source /home/daniel.monyak/.bashrc
+conda activate venv_tf_new
---
+++ source/tf2/masif_ligand/stochastic/train_vars.py
@@ -0,0 +1,5 @@
+train_vars = {}
+train_vars["continue_training"] = True
+train_vars["ckpPath"] = "kerasModel/ckp"
+train_vars["starting_iteration"] = 1000
+train_vars["num_iterations"] = 10000
