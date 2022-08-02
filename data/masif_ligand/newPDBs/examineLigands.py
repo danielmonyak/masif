e7db80827e72224f8f2975f80e248e0b28345ae4
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-08-02 16:48:41 -0400

    Update examineLigands.py

--- data/masif_ligand/newPDBs/examineLigands.py
+++ data/masif_ligand/newPDBs/examineLigands.py
@@ -4,3 +4,34 @@ import sys
 
 from SBI.structure import PDB
 from default_config.masif_opts import masif_opts
+
+params = masif_opts["ligand"]
+all_ligands = masif_opts['all_ligands']
+reg_ligands = masif_opts['ligand_site']
+solvents =  = masif_opts['solvents']
+
+all_lig_count = 0
+reg_lig_count = 0
+solvents_count = 0
+
+bad_pdbs = []
+
+#pdb_list = np.loadtxt('filtered_pdbs.txt', dtype=str)
+pdb_files = os.listdir('/data02/daniel/masif/masif_ligand/data_preparation/00b-pdbs_assembly')
+
+for fi in pdb_files:
+    try:
+        structure = PDB(
+            os.path.join(params["assembly_dir"], fi)
+        )
+    except:
+        bad_pdbs.append(fi)
+        continue
+    for chain in structure.chains:
+        for het in chain.heteroatoms
+            if het.type in all_ligands:
+                all_lig_count += 1
+                if het.type in reg_ligands:
+                    reg_lig_count += 1
+                if het.type in solvents:
+                    solvents_count += 1
