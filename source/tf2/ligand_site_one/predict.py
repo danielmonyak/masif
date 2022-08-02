78e07cdb10e130c967652bd05d3f4f6655ab3ce9
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-08-02 11:27:04 -0400

    Update predict.py

--- source/tf2/ligand_site_one/predict.py
+++ source/tf2/ligand_site_one/predict.py
@@ -43,6 +43,7 @@ def predict(model, pdb, threshold=0.5, min_size=50, make_y=True):
     resolution = 1. / params['scale']
     density = tfbio.data.make_grid(xyz_coords, probs[0], max_dist=params['max_dist'], grid_resolution=resolution)
 
+    centroid = xyz_coords.mean(axis=0)
     origin = (centroid - params['max_dist'])
     step = np.array([1.0 / params['scale']] * 3)
 
