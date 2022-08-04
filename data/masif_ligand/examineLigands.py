1e22a43aaef88e96d7579d0c35aedfd459cfadfd
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-08-04 12:51:48 -0400

    Update examineLigands.py

--- data/masif_ligand/examineLigands.py
+++ data/masif_ligand/examineLigands.py
@@ -5,12 +5,15 @@ from default_config.util import *
 import pickle
 
 params = masif_opts["ligand"]
-ligand_list = masif_opts['all_ligands']
+#ligand_list = masif_opts['all_ligands']
+ligand_list = masif_opts['ligand_list']
+
 pos_dists = [3.0, 5.0, 7.0, 9.0]
 
 freq_dict = dict(zip(ligand_list, [dict(zip(pos_dists, [[] for j in range(len(pos_dists))])) for i in range(len(ligand_list))]))
 
-pdb_list = os.listdir(params["masif_precomputation_dir"])
+#pdb_list = os.listdir(params["masif_precomputation_dir"])
+pdb_list = np.loadtxt('using_pdbs_final_reg.txt', dtype=str)
 
 pdbs_normal_ligands = 0
 
@@ -56,7 +59,7 @@ for k, pdb_id in enumerate(pdb_list):
         if structure_ligand in masif_opts['ligand_list']:
             normal_ligands = 1
         
-        '''if not structure_ligand in ligand_list:
+        if not structure_ligand in ligand_list:
             wrong_ligands.append(pdb_id)
             continue
 
@@ -67,18 +70,17 @@ for k, pdb_id in enumerate(pdb_list):
             temp_pocket_points = tree.query_ball_point(ligand_coords, dist)
             temp_pocket_points = list(set([pp for p in temp_pocket_points for pp in p]))
             temp_npoints = len(temp_pocket_points)
-            freq_dict[structure_ligand][dist].append(temp_npoints)'''
+            freq_dict[structure_ligand][dist].append(temp_npoints)
     
     pdbs_normal_ligands += normal_ligands
 
-'''
+
 with open('freq_dict.pickle', 'wb') as handle:
     pickle.dump(freq_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 np.save('bad_coords.npy', bad_coords)
 np.save('wrong_ligands.npy', wrong_ligands)
 np.save('no_precomp.npy', no_precomp)
-'''
 
 '''
 with open('freq_dict.pickle', 'rb') as handle:
