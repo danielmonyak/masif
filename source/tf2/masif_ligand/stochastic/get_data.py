f3bbcd0df23a0236b422c5c97a2ebf98297a9ba6
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-08-04 14:50:37 -0400

    Update get_data.py

--- source/tf2/masif_ligand/stochastic/get_data.py
+++ source/tf2/masif_ligand/stochastic/get_data.py
@@ -7,7 +7,7 @@ params = masif_opts["ligand"]
 minPockets = params['minPockets']
 #
 
-from time import time
+#from time import time
 
 def get_data(pdb_id, include_solvents=True):
     if include_solvents:
@@ -18,7 +18,7 @@ def get_data(pdb_id, include_solvents=True):
     mydir = os.path.join(params["masif_precomputation_dir"], pdb_id.rstrip('_') + '_')
     
     try:
-        before = time()
+#        before = time()
         
         input_feat = np.load(os.path.join(mydir, "p1_input_feat.npy"))
         rho_coords = np.load(os.path.join(mydir, "p1_rho_wrt_center.npy"))
@@ -34,7 +34,7 @@ def get_data(pdb_id, include_solvents=True):
                 params['ligand_coords_dir'], "{}_ligand_types.npy".format(pdb_id.split("_")[0])
             )
         ).astype(str)
-        print('import data %.4f' % (time() - before))
+#        print('import data %.4f' % (time() - before))
     except:
         return None
     
@@ -45,7 +45,7 @@ def get_data(pdb_id, include_solvents=True):
     
     ###############################################################
     ###############################################################
-    import_2_time = -time()
+#    import_2_time = -time()
     
     X_coords = np.load(os.path.join(mydir, "p1_X.npy"))
     Y_coords = np.load(os.path.join(mydir, "p1_Y.npy"))
@@ -62,10 +62,10 @@ def get_data(pdb_id, include_solvents=True):
         return None
     '''
     
-    import_2_time += time()
-    print('import data 2 %.4f' % import_2_time)
+#    import_2_time += time()
+#    print('import data 2 %.4f' % import_2_time)
     
-    prep_pp_time = -time()
+#    prep_pp_time = -time()
     
     pocket_points = []
     y = []
@@ -87,8 +87,8 @@ def get_data(pdb_id, include_solvents=True):
             y.append(ligIdx)
     
     
-    prep_pp_time += time()
-    print('prep_pp_time %.4f' % prep_pp_time)
+#    prep_pp_time += time()
+#    print('prep_pp_time %.4f' % prep_pp_time)
     
     if len(pocket_points) == 0:
         #print(f'{pdb_id} has no pockets big enough...')
