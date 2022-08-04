3d10649ec4c6d2ce5f5d27cde55294dbe7cdc168
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-08-04 14:45:01 -0400

    Update prep_data_fromDirs.py

--- source/tf2/masif_ligand/batch/prep_data_fromDirs.py
+++ source/tf2/masif_ligand/batch/prep_data_fromDirs.py
@@ -1,51 +1,22 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
-import sys
 import numpy as np
-from scipy import spatial
-from random import shuffle
+
+phys_gpus = tf.config.list_physical_devices('GPU')
+for phys_g in phys_gpus:
+    tf.config.experimental.set_memory_growth(phys_g, True)
 
 import default_config.util as util
 from default_config.masif_opts import masif_opts
-import tensorflow as tf
+from tf2.masif_ligand.stochastic.get_data import get_data
 
 params = masif_opts["ligand"]
 
-include_solvents = 
+include_solvents = False
 
-####
-#ligand_list = masif_opts['ligand_list']
-ligand_list = masif_opts['all_ligands']
-n_classes = len(ligand_list)
-####
-
-
-train_list = np.load('/home/daniel.monyak/software/masif/data/masif_ligand/newPDBs/lists/train_pdbs.npy')
-val_list = np.load('/home/daniel.monyak/software/masif/data/masif_ligand/newPDBs/lists/val_pdbs.npy')
-
-
-# Edited by Daniel Monyak
-train_pdbs = np.char.rstrip(train_pdbs, '_')
-val_pdbs = np.char.rstrip(val_pdbs, '_')
-test_pdbs = np.char.rstrip(test_pdbs, '_')
-####
-
-####
-listdir = 'extraLigands_lists'
-if not os.path.exists(listdir):
-    os.mkdir(listdir)
-####
-
-
-# Uncommented these save statements so that the lists would be redone
-np.save(os.path.join(listdir, 'train_pdbs_sequence.npy'), train_pdbs)
-np.save(os.path.join(listdir, 'val_pdbs_sequence.npy'), val_pdbs)
-np.save(os.path.join(listdir, 'test_pdbs_sequence.npy'), test_pdbs)
-
-# For this run use the train, validation and test sets actually used
-train_pdbs = np.load(os.path.join(listdir, 'train_pdbs_sequence.npy')).astype(str)
-val_pdbs = np.load(os.path.join(listdir, 'val_pdbs_sequence.npy')).astype(str)
-test_pdbs = np.load(os.path.join(listdir, 'test_pdbs_sequence.npy')).astype(str)
+train_pdbs = np.load('/home/daniel.monyak/software/masif/data/masif_ligand/newPDBs/lists/train_reg.npy')
+val_pdbs = np.load('/home/daniel.monyak/software/masif/data/masif_ligand/newPDBs/lists/val_reg.npy')
+test_pdbs = np.load('/home/daniel.monyak/software/masif/data/masif_ligand/newPDBs/lists/test_reg.npy')
 
 precom_dir = params["masif_precomputation_dir"]
 ligand_coord_dir = params["ligand_coords_dir"]
@@ -53,18 +24,18 @@ ligand_coord_dir = params["ligand_coords_dir"]
 defaultCode = params['defaultCode']
 minPockets = params['minPockets']
 
-outdir = '/data02/daniel/masif/datasets/tf2/masif_ligand/extraLigands'
+outdir = '/data02/daniel/masif/datasets/tf2/masif_ligand/allReg'
 if not os.path.exists(outdir):
     os.mkdir(outdir)
 
 genOutPath = os.path.join(outdir, '{}_{}.npy')
 
-def helper(feed_dict):
-    flat_list = list(map(lambda tsr_key : feed_dict[tsr_key].flatten(), data_order))
+def helper(X_dict):
+    flat_list = list(map(lambda tsr_key : X_dict[tsr_key].flatten(), data_order))
     return np.concatenate(flat_list, axis = 0)
 
-def compile_and_save(feed_list, y_list, dataset, len_list):
-    tsr_list = list(map(helper, feed_list))
+def compile_and_save(X_list, y_list, dataset, len_list):
+    tsr_list = list(map(helper, X_list))
     #
     X = np.empty([len(tsr_list), max(len_list)])
     X.fill(defaultCode)
@@ -79,80 +50,32 @@ def compile_and_save(feed_list, y_list, dataset, len_list):
 dataset_list = {'train' : train_pdbs, 'val' : val_pdbs, 'test' : test_pdbs}
 
 for dataset in dataset_list.keys():
-    i = 0
-    
-    feed_list = []
+    X_list = []
     y_list = []
     len_list = []
 
     temp_data = dataset_list[dataset]
     n_pdbs = len(temp_data)
-    for i, pdb in enumerate(temp_data):
+    for i, pdb_id in enumerate(temp_data):
         print(f'{dataset} record {i+1} of {n_pdbs}, {pdb}')
-        try:
-            # Load precomputed data
-            input_feat = np.load(
-                os.path.join(precom_dir, pdb + "_", "p1_input_feat.npy")
-            )
-            rho_wrt_center = np.load(
-                os.path.join(precom_dir, pdb + "_", "p1_rho_wrt_center.npy")
-            )
-            theta_wrt_center = np.load(
-                os.path.join(precom_dir, pdb + "_", "p1_theta_wrt_center.npy")
-            )
-            mask = np.load(
-                os.path.join(precom_dir, pdb + "_", "p1_mask.npy")
-            )
-            X = np.load(os.path.join(precom_dir, pdb + "_", "p1_X.npy"))
-            Y = np.load(os.path.join(precom_dir, pdb + "_", "p1_Y.npy"))
-            Z = np.load(os.path.join(precom_dir, pdb + "_", "p1_Z.npy"))
-            all_ligand_coords = np.load(
-                os.path.join(
-                    ligand_coord_dir, "{}_ligand_coords.npy".format(pdb.split("_")[0])
-                )
-            )
-            all_ligand_types = np.load(
-                os.path.join(
-                    ligand_coord_dir, "{}_ligand_types.npy".format(pdb.split("_")[0])
-                )
-            ).astype(str)
-        except:
+        data = get_data(pdb_id, include_solvents)
+        if data is None:
             continue
         
-        if len(all_ligand_types) == 0:
-            continue  
-        
-        xyz_coords = np.vstack([X, Y, Z]).T
-        tree = spatial.KDTree(xyz_coords)
-        pocket_labels = np.zeros(
-            (xyz_coords.shape[0], len(all_ligand_types)), dtype=np.int32
-        )
-        # Label points on surface within 3A distance from ligand with corresponding ligand type
-        for j, structure_ligand in enumerate(all_ligand_types):
-            ligand_coords = all_ligand_coords[j]
-            pocket_points = tree.query_ball_point(ligand_coords, angstrom_dist)
-            pocket_points = list(set([pp for p in pocket_points for pp in p]))
-            npoints = len(pocket_points)
-            
-            if npoints < minPockets:
-                continue
-            #
-            label = ligand_list.index(structure_ligand)
-            print(f'Ligand: {label}')
-            #
-            pocket_labels = np.zeros(n_classes, dtype=np.float32)
-            pocket_labels[label] = 1.0
-            #
-            feed_dict = {
-                'input_feat' : input_feat[pocket_points],
-                'rho_coords' : rho_wrt_center[pocket_points],
-                'theta_coords' : theta_wrt_center[pocket_points],
-                'mask' : mask[pocket_points]
+        X, pocket_points, y = data
+        for k, pp in enumerate(pocket_points):
+            y_temp = y[k]
+            X_dict = {
+                'input_feat' : X[0][pp],
+                'rho_coords' : X[1][pp],
+                'theta_coords' : X[2][pp],
+                'mask' : X[3][pp]
             }
-            feed_list.append(feed_dict)
-            y_list.append(pocket_labels)
-            len_list.append(feed_dict['input_feat'].size + feed_dict['rho_coords'].size + feed_dict['theta_coords'].size + feed_dict['mask'].size)
+            
+            X_list.append(X_dict)
+            y_list.append(y_temp)
+            len_list.append(X_dict['input_feat'].size + X_dict['rho_coords'].size + X_dict['theta_coords'].size + X_dict['mask'].size)
         
-    compile_and_save(feed_list, y_list, dataset, len_list)
+    compile_and_save(X_list, y_list, dataset, len_list)
 
 print('Finished!')
