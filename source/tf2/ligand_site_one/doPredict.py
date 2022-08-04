c550e166031e722bccc0027ab8c50da9121a7013
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-08-04 16:12:37 -0400

    Update doPredict.py

--- source/tf2/ligand_site_one/doPredict.py
+++ source/tf2/ligand_site_one/doPredict.py
@@ -28,12 +28,12 @@ except:
     sys.exit('Please enter a valid response...')
 
 if mode == 'pdb_id':
-    pdb = input(f'Enter pdb: ')
-    if pdb == '':
+    func_input = input(f'Enter pdb: ')
+    if func_input == '':
         sys.exit('Must enter a valid pdb...')
 else:
-    pdb_path = input(f'Enter path to precomputation directory of PDB: ')
-    if not os.path.exists(pdb_path):
+    func_input = input(f'Enter path to precomputation directory of PDB: ')
+    if not os.path.exists(func_input):
         sys.exit('Must enter a valid path...')
 
 modelDir = 'kerasModel'
@@ -88,7 +88,7 @@ if os.path.exists(outdir):
     _ = os.system(f'rm -r {outdir}')
 os.mkdir(outdir)
 
-ligand_coords_arr = predict(model, pdb, threshold, min_size, make_y, mode)
+ligand_coords_arr = predict(model, func_input, threshold, min_size, make_y, mode)
 for i, indices in enumerate(ligand_coords_arr):
     mol=openbabel.OBMol()
     for idx in indices:
