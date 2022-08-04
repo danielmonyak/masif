6e842e5f82150be91678bfc818f83f25d9000c4b
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-08-04 14:24:44 -0400

    Update get_data.py

--- source/tf2/LSResNet/stochastic/get_data.py
+++ source/tf2/LSResNet/stochastic/get_data.py
@@ -57,6 +57,9 @@ def get_data(pdb_id, training=True, include_solvents=True):
     
     pocket_points = []
     for j, structure_ligand in enumerate(all_ligand_types):
+        if not structure_ligand in ligand_list:
+            continue
+
         ligIdx = ligand_list.index(structure_ligand)
         if ligIdx < 7:
             dist = 3.0
