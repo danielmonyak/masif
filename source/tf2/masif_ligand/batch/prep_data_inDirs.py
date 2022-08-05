2bb7f208897450444378f543bc4a1c02b13e3942
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-08-05 00:16:43 -0400

    Update prep_data_inDirs.py

--- source/tf2/masif_ligand/batch/prep_data_inDirs.py
+++ source/tf2/masif_ligand/batch/prep_data_inDirs.py
@@ -5,10 +5,9 @@ from default_config.masif_opts import masif_opts
 from tf2.masif_ligand.stochastic.get_data import get_data
 
 params = masif_opts["ligand"]
+precom_dir = params["masif_precomputation_dir"]
 
 include_solvents = True
-
-precom_dir = params["masif_precomputation_dir"]
 
 all_pdbs = np.loadtxt('/home/daniel.monyak/software/masif/data/masif_ligand/newPDBs/filtered_pdbs.txt', dtype=str)
 n_pdbs = len(all_pdbs)
@@ -23,6 +22,9 @@ for i, pdb_id in enumerate(all_pdbs):
     for k, pp in enumerate(pocket_points):
         y_temp = y[k]
         X_temp = [arr[:, pp] for arr in X]
+        
+        X_temp[0] = np.squeeze(X_temp[0], 0)
+        X_temp[3] = np.squeeze(X_temp[3], 0)
 
         np.save(os.path.join(mydir, f'X_{k}.npy'), X_temp)
         np.save(os.path.join(mydir, f'y_{k}.npy'), y_temp)
