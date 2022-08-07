6ea63634493066d11d02a5244fc9bc8127c4499e
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-08-07 16:24:13 -0400

    Update get_data.py

--- source/tf2/LSResNet/get_data.py
+++ source/tf2/LSResNet/get_data.py
@@ -67,8 +67,9 @@ def get_data(func_input, training = True, make_y = True, mode='pdb_id', include_
 
         pocket_points = []
         for j, structure_ligand in enumerate(all_ligand_types):
-            #if not structure_ligand in ligand_list:
-            #    continue
+            if not structure_ligand in ligand_list:
+                continue
+
             ligand_coords = all_ligand_coords[j]
             temp_pocket_points = tree.query_ball_point(ligand_coords, 3.0)
             temp_pocket_points = list(set([pp for p in temp_pocket_points for pp in p]))
