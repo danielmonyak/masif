dad634c0aeabe5e025663b44bf7e9db934591936
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-08-06 22:33:18 -0400

    Update train_model.py

--- source/tf2/LSResNet/hinge/train_model.py
+++ source/tf2/LSResNet/hinge/train_model.py
@@ -42,119 +42,121 @@ print(f'Training for {num_epochs} epochs')
 if continue_training:
     print(f'Resuming training from checkpoint at {ckpPath}, starting at epoch {starting_epoch}')
 
-model = LSResNet(
-    params["max_distance"],
-    feat_mask=params["feat_mask"],
-    n_thetas=4,
-    n_rhos=3,
-    learning_rate = lr,
-    n_rotations=4,
-    reg_val = 0,
-    use_special_neuron = True,
-    reg_const = 1e-2
-)
-model.compile(optimizer = 'adam')
-
-'''
-hinge_inst = losses.Hinge()
-hinge_p = 3
-def polynomialHingeLoss(y, y_pred):
-    return tf.pow(hinge_inst(y, y_pred), hinge_p)
-
-model.compile(optimizer = 'adam',
-             loss = polynomialHingeLoss,
-             metrics = [F1, )
-'''
-
-if continue_training:
-    model.load_weights(ckpPath)
-    print(f'Loaded model from {ckpPath}')
-
-training_list = np.load('/home/daniel.monyak/software/masif/data/masif_ligand/lists/train_pdbs_sequence.npy')
-val_list = np.load('/home/daniel.monyak/software/masif/data/masif_ligand/lists/val_pdbs_sequence.npy')
-
-#######################################
-#######################################
-#######################################
-
-cur_batch_sz = 0
-for i in range(num_epochs):
-    if i < starting_epoch:
-        continue
+with tf.device('/GPU:1'):
     
-    train_j = 0
-    batch_i = 0
-    #############################################################
-    ###################     TRAINING DATA     ###################
-    #############################################################
-    
-    np.random.shuffle(training_list)
-    
-    loss_list = []
-    acc_list = []
-    F1_list = []
-    for pdb_id in training_list:
-        data = get_data(pdb_id)
-        
-        if data is None:
+    model = LSResNet(
+        params["max_distance"],
+        feat_mask=params["feat_mask"],
+        n_thetas=4,
+        n_rhos=3,
+        learning_rate = lr,
+        n_rotations=4,
+        reg_val = 0,
+        use_special_neuron = True,
+        reg_const = 1e-2
+    )
+    model.compile(optimizer = 'adam')
+
+    '''
+    hinge_inst = losses.Hinge()
+    hinge_p = 3
+    def polynomialHingeLoss(y, y_pred):
+        return tf.pow(hinge_inst(y, y_pred), hinge_p)
+
+    model.compile(optimizer = 'adam',
+                 loss = polynomialHingeLoss,
+                 metrics = [F1, )
+    '''
+
+    if continue_training:
+        model.load_weights(ckpPath)
+        print(f'Loaded model from {ckpPath}')
+
+    training_list = np.load('/home/daniel.monyak/software/masif/data/masif_ligand/lists/train_pdbs_sequence.npy')
+    val_list = np.load('/home/daniel.monyak/software/masif/data/masif_ligand/lists/val_pdbs_sequence.npy')
+
+    #######################################
+    #######################################
+    #######################################
+
+    cur_batch_sz = 0
+    for i in range(num_epochs):
+        if i < starting_epoch:
             continue
-        
-        #X, y = data
-        dataset_temp = tf.data.Dataset.from_tensors(data)
-        if cur_batch_sz == 0:
-            dataset = dataset_temp
-            cur_batch_sz += 1
-        else:
-            dataset = dataset.concatenate(dataset_temp)
-            cur_batch_sz += 1
-            if cur_batch_sz == train_batch_sz_threshold:
-                print(f'Epoch {i}, training on {cur_batch_sz} pdbs, batch {batch_i}')
-                
-                history = model.fit(dataset, verbose = 2)
-                loss_list.extend(history.history['loss'])
-                acc_list.extend(history.history['hinge_accuracy'])
-                F1_list.extend(history.history['F1'])
-                
-                cur_batch_sz = 0
-                batch_i += 1
-        
-        train_j += 1
-    
-    if cur_batch_sz > 0:
-        print(f'Epoch {i}, training on {cur_batch_sz} pdbs, batch {batch_i}')
-        
-        history = model.fit(dataset, verbose = 2)
-        loss_list.extend(history.history['loss'])
-        acc_list.extend(history.history['hinge_accuracy'])
-        F1_list.extend(history.history['F1'])
-        
-        cur_batch_sz = 0
-    
-    print(f'\nEpoch {i}, Training Metrics')
-    print(f'Loss: {np.mean(loss_list)}')
-    print(f'Hinge Accuracy: {np.mean(acc_list)}')
-    print(f'F1: {np.mean(F1_list)}\n')
-    
-    loss_list = []
-    acc_list = []
-    F1_list = []
-    for pdb_id in val_list:
-        data = get_data(pdb_id)
-        if data is None:
-            continue
-            
-        X, y = data
-        loss, f1, acc = model.evaluate(X, y, verbose=0)
-        loss_list.append(loss)
-        acc_list.append(acc)
-        F1_list.append(f1)
-    
-    print(f'\nEpoch {i}, Validation Metrics')
-    print(f'Loss: {np.mean(loss_list)}')
-    print(f'Hinge Accuracy: {np.mean(acc_list)}')
-    print(f'F1: {np.mean(F1_list)}\n')
-    
-    print(f'Saving model weights to {ckpPath}')
-    model.save_weights(ckpPath)
 
-print(f'Finished {num_epochs} training epochs!')
+        train_j = 0
+        batch_i = 0
+        #############################################################
+        ###################     TRAINING DATA     ###################
+        #############################################################
+
+        np.random.shuffle(training_list)
+
+        loss_list = []
+        acc_list = []
+        F1_list = []
+        for pdb_id in training_list:
+            data = get_data(pdb_id)
+
+            if data is None:
+                continue
+
+            #X, y = data
+            dataset_temp = tf.data.Dataset.from_tensors(data)
+            if cur_batch_sz == 0:
+                dataset = dataset_temp
+                cur_batch_sz += 1
+            else:
+                dataset = dataset.concatenate(dataset_temp)
+                cur_batch_sz += 1
+                if cur_batch_sz == train_batch_sz_threshold:
+                    print(f'Epoch {i}, training on {cur_batch_sz} pdbs, batch {batch_i}')
+
+                    history = model.fit(dataset, verbose = 2)
+                    loss_list.extend(history.history['loss'])
+                    acc_list.extend(history.history['hinge_accuracy'])
+                    F1_list.extend(history.history['F1'])
+
+                    cur_batch_sz = 0
+                    batch_i += 1
+
+            train_j += 1
+
+        if cur_batch_sz > 0:
+            print(f'Epoch {i}, training on {cur_batch_sz} pdbs, batch {batch_i}')
+
+            history = model.fit(dataset, verbose = 2)
+            loss_list.extend(history.history['loss'])
+            acc_list.extend(history.history['hinge_accuracy'])
+            F1_list.extend(history.history['F1'])
+
+            cur_batch_sz = 0
+
+        print(f'\nEpoch {i}, Training Metrics')
+        print(f'Loss: {np.mean(loss_list)}')
+        print(f'Hinge Accuracy: {np.mean(acc_list)}')
+        print(f'F1: {np.mean(F1_list)}\n')
+
+        loss_list = []
+        acc_list = []
+        F1_list = []
+        for pdb_id in val_list:
+            data = get_data(pdb_id)
+            if data is None:
+                continue
+
+            X, y = data
+            loss, f1, acc = model.evaluate(X, y, verbose=0)
+            loss_list.append(loss)
+            acc_list.append(acc)
+            F1_list.append(f1)
+
+        print(f'\nEpoch {i}, Validation Metrics')
+        print(f'Loss: {np.mean(loss_list)}')
+        print(f'Hinge Accuracy: {np.mean(acc_list)}')
+        print(f'F1: {np.mean(F1_list)}\n')
+
+        print(f'Saving model weights to {ckpPath}')
+        model.save_weights(ckpPath)
+
+    print(f'Finished {num_epochs} training epochs!')
