7a2c4c1354c8bfcce1e79fe16b830e664844d255
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-08-09 15:06:51 -0400

    Update util.py

--- source/default_config/util.py
+++ source/default_config/util.py
@@ -1,3 +1,5 @@
+# Useful constants, functions, and classes
+
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import metrics
@@ -5,10 +7,10 @@ from default_config.masif_opts import masif_opts
 
 minPockets = masif_opts['ligand']['minPockets']
 
+# Used in MaSIF_ligand model
 inputFeatSpec = tf.RaggedTensorSpec(shape=[None, 200, 5], dtype=tf.float32)
 restSpec = tf.RaggedTensorSpec(shape=[None, 200, 1], dtype=tf.float32)
 sampleSpec = tf.TensorSpec([minPockets], dtype=tf.int32)
-
 inputFeatSpecTsr = tf.TensorSpec(shape=[200, 5], dtype=tf.float32)
 restSpecTsr = tf.RaggedTensorSpec(shape=[200, 1], dtype=tf.float32)
 
@@ -19,12 +21,14 @@ data_order = ['input_feat', 'rho_coords', 'theta_coords', 'mask']
 
 flatten = lambda a : tf.reshape(a, [-1])
 
+# Custom intializer to use in "self.add_weight"
 class ValueInit(tf.keras.initializers.Initializer):
   def __init__(self, value):
     self.value = value
   def __call__(self, shape, dtype=None, **kwargs):
     return self.value
 
+# Pad indices array with value of index of that row
 def pad_indices(indices, max_verts):
     ret_list = []
     for patch_ix in range(len(indices)):
@@ -33,6 +37,7 @@ def pad_indices(indices, max_verts):
         )
     return np.stack(ret_list)
 
+'''
 def F1(y_true, y_pred, threshold=0.5):
     y_true = tf.cast(y_true, dtype=tf.bool)
     y_pred = tf.sigmoid(y_pred) > threshold
@@ -43,7 +48,37 @@ def F1(y_true, y_pred, threshold=0.5):
     precision = overlap/n_pred
     f1 = 2*precision*recall / (precision + recall)
     return tf.where(tf.math.is_nan(f1), tf.zeros_like(f1), f1)
+'''
 
+# Custom Keras metric to calculate F1 score
+class F1_Metric(metrics.Metric):
+    def __init__(self, name='F1', from_logits=False, threshold=0.5, **kwargs):
+        super(F1_Metric, self).__init__(name=name, **kwargs)
+        self.f1_score = self.add_weight(name='f1_score', initializer='zeros')
+        self.total = self.add_weight(name='total', initializer='zeros')
+        self.count = self.add_weight(name='count', initializer='zeros')
+        self.from_logits = from_logits
+        self.threshold = threshold
+    def update_state(self, y_true, y_pred):
+        y_true = tf.cast(y_true, dtype=tf.float32)
+        if self.from_logits:
+            y_pred = tf.sigmoid(y_pred)
+        y_true = tf.reshape(y_true, [-1]) > self.threshold
+        y_pred = tf.reshape(y_pred, [-1]) > self.threshold
+        
+        overlap = tf.reduce_sum(tf.cast(y_true & y_pred, dtype=tf.float32))
+        n_true = tf.reduce_sum(tf.cast(y_true, dtype=tf.float32))
+        n_pred = tf.reduce_sum(tf.cast(y_pred, dtype=tf.float32))
+        recall = overlap/n_true
+        precision = overlap/n_pred
+        f1 = 2*precision*recall / (precision + recall)
+        f1 = tf.where(tf.math.is_nan(f1), tf.zeros_like(f1), f1)
+        self.total.assign_add(tf.cast(f1, self.dtype))
+        self.count.assign_add(tf.cast(1, self.dtype))
+    def result(self):
+        return self.total/self.count
+
+# Custom Keras metric to calculate accuracy when using the hinge-loss model
 class HingeAccuracy(metrics.Metric):
     def __init__(self, name='hinge_accuracy', **kwargs):
         super(HingeAccuracy, self).__init__(name=name, **kwargs)
@@ -62,6 +97,7 @@ class HingeAccuracy(metrics.Metric):
         self.hinge_acc_score = self.n_matching/self.n_total
         return self.hinge_acc_score
 
+# Custom Keras metric to calculate F1 score when using the hinge-loss model
 class F1_Metric_Hinge(metrics.Metric):
     def __init__(self, name='F1', **kwargs):
         super(F1_Metric, self).__init__(name=name, **kwargs)
@@ -72,34 +108,6 @@ class F1_Metric_Hinge(metrics.Metric):
         y_true = tf.cast(y_true, dtype=tf.float32)
         y_true = tf.reshape(y_true, [-1]) > 0.0
         y_pred = tf.reshape(y_pred, [-1]) > 0.0
-        overlap = tf.reduce_sum(tf.cast(y_true & y_pred, dtype=tf.float32))
-        n_true = tf.reduce_sum(tf.cast(y_true, dtype=tf.float32))
-        n_pred = tf.reduce_sum(tf.cast(y_pred, dtype=tf.float32))
-        recall = overlap/n_true
-        precision = overlap/n_pred
-        f1 = 2*precision*recall / (precision + recall)
-        f1 = tf.where(tf.math.is_nan(f1), tf.zeros_like(f1), f1)
-        self.total.assign_add(tf.cast(f1, self.dtype))
-        self.count.assign_add(tf.cast(1, self.dtype))
-    def result(self):
-        return self.total/self.count
-
-  
-class F1_Metric(metrics.Metric):
-    def __init__(self, name='F1', from_logits=False, threshold=0.5, **kwargs):
-        super(F1_Metric, self).__init__(name=name, **kwargs)
-        self.f1_score = self.add_weight(name='f1_score', initializer='zeros')
-        self.total = self.add_weight(name='total', initializer='zeros')
-        self.count = self.add_weight(name='count', initializer='zeros')
-        self.from_logits = from_logits
-        self.threshold = threshold
-    def update_state(self, y_true, y_pred):
-        y_true = tf.cast(y_true, dtype=tf.float32)
-        if self.from_logits:
-            y_pred = tf.sigmoid(y_pred)
-        y_true = tf.reshape(y_true, [-1]) > self.threshold
-        y_pred = tf.reshape(y_pred, [-1]) > self.threshold
-        
         overlap = tf.reduce_sum(tf.cast(y_true & y_pred, dtype=tf.float32))
         n_true = tf.reduce_sum(tf.cast(y_true, dtype=tf.float32))
         n_pred = tf.reduce_sum(tf.cast(y_pred, dtype=tf.float32))
