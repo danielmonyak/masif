275d86a149250ae41970f1822eeb44a5b9787465
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-08-09 10:52:43 -0400

    Update train_model.py

--- source/tf2/masif_ligand/train_model.py
+++ source/tf2/masif_ligand/train_model.py
@@ -15,7 +15,24 @@ from tf2.masif_ligand.MaSIF_ligand_TF2 import MaSIF_ligand
 reg_val = 0.0
 reg_type = 'l2'
 
-continue_training = False
+##########################################
+##########################################
+with open('train_vars.pickle', 'rb') as handle:
+    train_vars = pickle.load(handle)
+
+continue_training = train_vars['continue_training']
+ckpPath = train_vars['ckpPath']
+num_epochs = train_vars['num_epochs']
+starting_epoch = train_vars['starting_epoch']
+lr = train_vars['lr']
+
+print(f'Training for {num_epochs} epochs')
+if continue_training:
+    print(f'Resuming training from checkpoint at {ckpPath}, starting at epoch {starting_epoch}, using learning rate {lr:.1e}')
+
+##########################################
+##########################################
+
 dev = '/GPU:3'
 cpu = '/CPU:0'
 
@@ -37,21 +54,20 @@ with tf.device(cpu):
 
 
 modelDir = 'kerasModel'
-ckpPath = os.path.join(modelDir, 'ckp')
 modelPath = os.path.join(modelDir, 'savedModel')
 
 gpus = tf.config.experimental.list_logical_devices('GPU')
 gpus_str = [g.name for g in gpus]
 strategy = tf.distribute.MirroredStrategy(gpus_str[1:3])
 
-num_epochs = 200
 #with tf.device(dev):
 with strategy.scope():
   model = MaSIF_ligand(
     params["max_distance"],
     params["n_classes"],
     feat_mask=params["feat_mask"],
-    reg_val = reg_val, reg_type = reg_type
+    reg_val = reg_val, reg_type = reg_type,
+    lr = lr
   )
   model.compile(optimizer = model.opt,
     loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
@@ -59,23 +75,16 @@ with strategy.scope():
   )  
   if continue_training:
     model.load_weights(ckpPath)
-    last_epoch = 18
-    initValThresh = 0.71429
-  else:
-    last_epoch = 0
-    initValThresh = 0
 
   saveCheckpoints = tf.keras.callbacks.ModelCheckpoint(
     ckpPath,
-    monitor = 'val_sparse_categorical_accuracy',
-    save_best_only = True,
-    verbose = 1,
-    initial_value_threshold = initValThresh
+    save_best_only = False,
+    verbose = 0
   )
 
   history = model.fit(x = train_X, y = train_y,
     epochs = num_epochs,
-    initial_epoch = last_epoch,
+    initial_epoch = starting_epoch,
     validation_data = (val_X, val_y),
     callbacks = [saveCheckpoints],
     verbose = 2,
