a2f0313e8bf77a3c5f784e60fd395428063ea56f
danielmonyak <daniel.monyak@arvinas.com>
2022-08-09 21:33:06 +0000

    update

--- source/tf2/masif_ligand/batch/evaluate.sh
+++ source/tf2/masif_ligand/batch/evaluate.sh
@@ -1,10 +1,10 @@
-source /apps01/anaconda3/etc/profile.d/conda.sh
-conda activate venv_tf
+source /home/daniel.monyak/miniconda3/etc/profile.d/conda.sh
+conda activate venv_tf_new
 
 masif_root=$(git rev-parse --show-toplevel)
 masif_source=$masif_root/source
 export PYTHONPATH=$PYTHONPATH:$masif_source
 
-python -u evaluate.py $1 $2 > $1/${2}_evaluate.out 2>evaluate.err &
+python -u evaluate.py > evaluate.out 2>evaluate.err &
 disown -h $!
 echo $! > evaluate_pid.txt
