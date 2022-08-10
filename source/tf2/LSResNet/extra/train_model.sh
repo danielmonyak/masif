2cbc45d11ea609fdef38aed2ae283c87a7f33847
danielmonyak <daniel.monyak@arvinas.com>
2022-08-10 18:42:15 +0000

    update

---
+++ source/tf2/LSResNet/extra/train_model.sh
@@ -0,0 +1,19 @@
+source /home/daniel.monyak/miniconda3/etc/profile.d/conda.sh
+conda activate venv_tf_new
+
+masif_root=$(git rev-parse --show-toplevel)
+masif_source=$masif_root/source
+export PYTHONPATH=$PYTHONPATH:$masif_source
+
+job_name=train_model
+
+script=${job_name}.py
+
+#####
+if [[ -f train_vars.pickle ]]; then rm train_vars.pickle; fi
+python $masif_source/tf2/input_helper_iters.py
+#####
+
+python -u $script > ${job_name}.out 2>${job_name}.err &
+disown -h $!
+echo $! > ${job_name}_pid.txt
