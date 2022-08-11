e586180b5e4aef98db8bb6de5b8c243e0e580bf5
danielmonyak <106276872+danielmonyak@users.noreply.github.com>
2022-08-11 15:05:18 -0400

    Update util.py

--- source/default_config/util.py
+++ source/default_config/util.py
@@ -58,8 +58,10 @@ def F1(y_true, y_pred, threshold=0.5):
 
 # Custom Keras metric to calculate F1 score
 class F1_Metric(metrics.Metric):
-    def __init__(self, name='F1', from_logits=False, threshold=0.5, **kwargs):
-        super(F1_Metric, self).__init__(name=f'{name}_{threshold}', **kwargs)
+    def __init__(self, name=None, from_logits=False, threshold=0.5, **kwargs):
+        if name is None:
+            name = f'F1_{threshold}'
+        super(F1_Metric, self).__init__(name=name, **kwargs)
         self.f1_score = self.add_weight(name='f1_score', initializer='zeros')
         self.total = self.add_weight(name='total', initializer='zeros')
         self.count = self.add_weight(name='count', initializer='zeros')
