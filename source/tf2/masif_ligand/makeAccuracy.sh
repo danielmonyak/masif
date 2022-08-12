6efb07e43c5996466bea3ed7819b8e9d4f97ceee
danielmonyak <daniel.monyak@arvinas.com>
2022-08-12 19:59:06 +0000

    update

--- source/tf2/masif_ligand/makeAccuracy.sh
+++
@@ -1,2 +0,0 @@
-pcregrep -o2 "( sparse_categorical_accuracy\: )([\.0-9]+)" train_model.out > accuracy.txt
-pcregrep -o2 "( val_sparse_categorical_accuracy\: )([\.0-9]+)" train_model.out > val_accuracy.txt
