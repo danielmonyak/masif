54a05336818fc8da089159f0584b811a52008aa5
danielmonyak <daniel.monyak@arvinas.com>
2022-08-12 02:13:45 +0000

    udpate

--- source/tf2/masif_ligand/train_model.py
+++ source/tf2/masif_ligand/train_model.py
@@ -13,7 +13,7 @@ import default_config.util as util
 from default_config.masif_opts import masif_opts
 from tf2.masif_ligand.MaSIF_ligand_TF2 import MaSIF_ligand
 
-reg_val = 0.0
+reg_val = 0.00001
 reg_type = 'l2'
 
 ##########################################
